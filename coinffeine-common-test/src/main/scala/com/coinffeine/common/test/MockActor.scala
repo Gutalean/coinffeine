dcf9a65a1370243a5caa1862662cc1cb968df491
gotoalberto <gotoalberto@coinffeine.com>
2014-07-07 11:06:41 +0200

    Add some scaladoc.

--- coinffeine-common-test/src/main/scala/com/coinffeine/common/test/MockActor.scala
+++ coinffeine-common-test/src/main/scala/com/coinffeine/common/test/MockActor.scala
@@ -3,6 +3,10 @@ package com.coinffeine.common.test
 import akka.actor._
 import akka.testkit.TestProbe
 
+/** This actor allow to forward messages and spy what it receives
+  *
+  * @param master who is informed about the actor activity
+  */
 class MockActor(master: ActorRef) extends Actor with ActorLogging {
 
   import com.coinffeine.common.test.MockActor._
@@ -27,10 +31,32 @@ object MockActor {
   def props(master: ActorRef): Props = Props(new MockActor(master))
   def props(probe: TestProbe): Props = props(probe.ref)
 
+  /** Notification to the master when the mock is started.
+    *
+    * @param ref The reference of the newly created actor
+    */
   case class MockStarted(ref: ActorRef)
+
+  /** Notification to the master when the mock is stopped.
+    *
+    * @param ref The reference of the stopped actor
+    */
   case class MockStopped(ref: ActorRef)
+
+  /** Notification to the master when the mock is restarted */
   case class MockRestarted(ref: ActorRef, reason: Throwable)
+
+  /** Notification sen to the master of the mock receiving any message.
+    *
+    * @param ref      The mock actor
+    * @param sender   Who is communicating with the mock
+    * @param message  Received message
+    */
   case class MockReceived(ref: ActorRef, sender: ActorRef, message: Any)
+
+  /** Message sent to the mock to make it resent as own */
   case class MockSend(target: ActorRef, message: Any)
+
+  /** Message sent to the mock to make it fail with an exception */
   case class MockThrow(ex: Throwable)
 }
