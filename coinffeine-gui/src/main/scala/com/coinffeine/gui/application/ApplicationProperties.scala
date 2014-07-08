ed5bd2a62559f15e82d261283795ab29f8ac83a6
Alvaro Polo <apoloval@gmail.com>
2014-07-08 14:47:50 +0200

    Add `ApplicationProperties` class to hold the properties of the app
    As the application have to act as a model for the UI, a property-based set
    of classes is needed. This change introduces `ApplicationProperties`, which
    wraps a `CoinffeineApp` object and exports some properties bound to the
    underlying data model using event handlers.

---
+++ coinffeine-gui/src/main/scala/com/coinffeine/gui/application/ApplicationProperties.scala
@@ -0,0 +1,21 @@
+package com.coinffeine.gui.application
+
+import scalafx.collections.ObservableBuffer
+
+import com.coinffeine.client.api.{CoinffeineApp, EventHandler}
+import com.coinffeine.gui.application.properties.OrderProperties
+import com.coinffeine.gui.util.FxEventHandler
+
+class ApplicationProperties(app: CoinffeineApp) {
+
+  val ordersProperty = ObservableBuffer[OrderProperties]()
+
+  private val eventHandler: EventHandler = FxEventHandler {
+    case CoinffeineApp.OrderSubmittedEvent(order) =>
+      ordersProperty.add(OrderProperties(order))
+    case CoinffeineApp.OrderCancelledEvent(order) =>
+      ordersProperty.remove(OrderProperties(order))
+  }
+
+  app.observe(eventHandler)
+}
---
+++ coinffeine-gui/src/main/scala/com/coinffeine/gui/util/FxEventHandler.scala
@@ -0,0 +1,32 @@
+package com.coinffeine.gui.util
+
+import scalafx.application.Platform
+
+import com.coinffeine.client.api.{CoinffeineApp, EventHandler}
+
+/** A event handler ready to be used in JavaFX/ScalaFX concurrency model.
+  *
+  * JavaFX/ScalaFX have a very restrictive concurrency model to avoid race conditions
+  * in its node tree. Thus, any event handler invoked from another thread other than
+  * the application thread may cause a system exception.
+  *
+  * This class wraps a EventHandler object and ensures it is executed in the application
+  * thread avoiding race conditions.
+  *
+  * @param delegate The event handler to be safely wrapped
+  */
+class FxEventHandler(delegate: EventHandler) extends EventHandler {
+
+  override def isDefinedAt(x: CoinffeineApp.Event) = delegate.isDefinedAt(x)
+
+  override def apply(event: CoinffeineApp.Event): Unit = {
+    Platform.runLater {
+      delegate(event)
+    }
+  }
+}
+
+object FxEventHandler {
+
+  def apply(delegate: EventHandler) = new FxEventHandler(delegate)
+}
