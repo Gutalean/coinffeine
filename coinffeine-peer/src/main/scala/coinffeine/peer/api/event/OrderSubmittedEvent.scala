ceaf298727616b7a3cb93f9267cb1d9d720f98cc
Alvaro Polo <apoloval@gmail.com>
2014-07-18 09:53:32 +0200

    Remove type parameters from order events
    The receiver of the events that report activity on orders cannot benefit from
    the event types to have type parameters. Since `Order` is covariant,
    `Order[FiatCurrency]` may be used as a supertype of all orders. This change
    removes the type parameters from the event types.

--- coinffeine-peer/src/main/scala/coinffeine/peer/api/event/OrderSubmittedEvent.scala
+++ coinffeine-peer/src/main/scala/coinffeine/peer/api/event/OrderSubmittedEvent.scala
@@ -4,5 +4,5 @@ import coinffeine.model.currency.FiatCurrency
 import coinffeine.model.market.Order
 
 /** An event triggered each time a new order is submitted. */
-case class OrderSubmittedEvent[+C <: FiatCurrency](order: Order[C]) extends CoinffeineAppEvent
+case class OrderSubmittedEvent(order: Order[FiatCurrency]) extends CoinffeineAppEvent
 
