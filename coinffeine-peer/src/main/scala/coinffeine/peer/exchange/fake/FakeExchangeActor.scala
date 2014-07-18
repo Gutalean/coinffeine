11665c882a381b6696dd717d89bb03b8d937dced
Sebastian Ortega <sortega@coinffeine.com>
2014-07-18 15:45:53 +0200

    Implement FakeExchangeActor

--- coinffeine-peer/src/main/scala/coinffeine/peer/exchange/fake/FakeExchangeActor.scala
+++ coinffeine-peer/src/main/scala/coinffeine/peer/exchange/fake/FakeExchangeActor.scala
@@ -1,13 +1,47 @@
 package coinffeine.peer.exchange.fake
 
-import akka.actor.{Props, Actor}
+import scala.concurrent.duration._
+
+import akka.actor._
 
 import coinffeine.peer.exchange.ExchangeActor
 
 /** Performs a fake exchange for demo purposes */
-class FakeExchangeActor extends Actor {
-  // TODO: not yet implemented
-  override def receive: Receive = ???
+class FakeExchangeActor extends Actor with ActorLogging {
+
+  override def receive: Receive = {
+    case init: ExchangeActor.StartExchange =>
+      new InitializedExchange(init, sender()).start()
+  }
+
+  private class InitializedExchange(init: ExchangeActor.StartExchange, listener: ActorRef) {
+    import init._
+
+    def start(): Unit = {
+      logEvent("Handshake success")
+      context.setReceiveTimeout(2.seconds)
+      context.become(handlingStep(0))
+    }
+
+    def handlingStep(step: Int): Receive = {
+      case ReceiveTimeout if step == exchange.amounts.breakdown.totalSteps =>
+        logEvent(s"Exchange finished successfully")
+        listener ! ExchangeActor.ExchangeSuccess
+        context.become(finished)
+
+      case ReceiveTimeout =>
+        logEvent(s"Step $step finished")
+        context.become(handlingStep(step + 1))
+    }
+
+    val finished: Receive = {
+      case _ =>
+    }
+
+    private def logEvent(message: String): Unit = {
+      log.info("{}: {}", exchange.id, message)
+    }
+  }
 }
 
 object FakeExchangeActor {
