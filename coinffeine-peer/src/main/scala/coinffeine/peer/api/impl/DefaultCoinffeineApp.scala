428fabebb0c1b8c4143969461b69210c28d792db
Sebastian Ortega <sortega@coinffeine.com>
2014-07-31 14:55:00 +0200

    Improve naming of integration test actor refs

--- coinffeine-peer/src/main/scala/coinffeine/peer/api/impl/DefaultCoinffeineApp.scala
+++ coinffeine-peer/src/main/scala/coinffeine/peer/api/impl/DefaultCoinffeineApp.scala
@@ -8,11 +8,18 @@ import coinffeine.peer.api._
 import coinffeine.peer.config.ConfigComponent
 import coinffeine.peer.event.EventObserverActor
 
-/** Implements the coinffeine application API as an actor system. */
-class DefaultCoinffeineApp(accountId: PaymentProcessor.AccountId,
+/** Implements the coinffeine application API as an actor system.
+  *
+  * @constructor
+  * @param name       Name used for the actor system (useful for making sense of actor refs)
+  * @param accountId  Payment processor account to use
+  * @param peerProps  Props to start the main actor
+  */
+class DefaultCoinffeineApp(name: String,
+                           accountId: PaymentProcessor.AccountId,
                            peerProps: Props) extends CoinffeineApp {
 
-  private val system = ActorSystem()
+  private val system = ActorSystem(name)
   private val peerRef = system.actorOf(peerProps, "peer")
 
   override val network = new DefaultCoinffeineNetwork(peerRef)
@@ -37,6 +44,6 @@ object DefaultCoinffeineApp {
 
     private val accountId = config.getString("coinffeine.okpay.id")
 
-    override lazy val app = new DefaultCoinffeineApp(accountId, peerProps)
+    override lazy val app = new DefaultCoinffeineApp(name = accountId, accountId, peerProps)
   }
 }
