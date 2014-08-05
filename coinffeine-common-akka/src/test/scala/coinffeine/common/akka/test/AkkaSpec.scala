353e0617476d383752f77029c9d3895e3c4c6c89
Sebastian Ortega <sortega@coinffeine.com>
2014-08-05 10:04:20 +0200

    Introduce FutureMatchers to replace ScalaFutures

--- coinffeine-common-akka/src/test/scala/coinffeine/common/akka/test/AkkaSpec.scala
+++ coinffeine-common-akka/src/test/scala/coinffeine/common/akka/test/AkkaSpec.scala
@@ -4,15 +4,15 @@ import akka.actor._
 import akka.testkit.{ImplicitSender, TestKit, TestKitExtension}
 import com.typesafe.config.ConfigFactory
 import org.scalatest._
-import org.scalatest.concurrent.ScalaFutures
+
+import coinffeine.common.test.FutureMatchers
 
 /** FlatSpec configured to test Akka actors. */
 abstract class AkkaSpec(actorSystem: ActorSystem = ActorSystem("TestSystem"))
   extends TestKit(actorSystem) with FlatSpecLike with BeforeAndAfterAll with ShouldMatchers
-  with ImplicitSender with ScalaFutures {
+  with ImplicitSender with FutureMatchers {
 
-  override def spanScaleFactor: Double =
-    TestKitExtension.get(system).TestTimeFactor
+  override def scaleFactor: Double = TestKitExtension.get(system).TestTimeFactor
 
   def this(systemName: String) = this(ActorSystem(systemName))
 
--- coinffeine-common-test/src/main/scala/coinffeine/common/test/UnitTest.scala
+++ coinffeine-common-test/src/main/scala/coinffeine/common/test/UnitTest.scala
@@ -2,10 +2,9 @@ package coinffeine.common.test
 
 import akka.actor.ActorSystem
 import akka.testkit.TestKitExtension
-import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfter, FlatSpecLike, ShouldMatchers}
 
 /** Default trait for unit tests that mixes the most typical testing traits. */
-trait UnitTest extends FlatSpecLike with ShouldMatchers with BeforeAndAfter with ScalaFutures {
-  override def spanScaleFactor = TestKitExtension.get(ActorSystem()).TestTimeFactor
+trait UnitTest extends FlatSpecLike with ShouldMatchers with BeforeAndAfter with FutureMatchers {
+  override def scaleFactor: Double = TestKitExtension.get(ActorSystem()).TestTimeFactor
 }
