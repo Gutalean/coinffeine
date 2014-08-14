55cfa5e118c6d83db13c009db0d964b8623bffd3
Sebastian Ortega <sortega@coinffeine.com>
2014-08-14 16:21:46 +0200

    Introduce EventChannelProbe
    It makes easier to assert on Coinffeine events produced

--- coinffeine-peer/src/test/scala/coinffeine/peer/bitcoin/BitcoinPeerActorTest.scala
+++ coinffeine-peer/src/test/scala/coinffeine/peer/bitcoin/BitcoinPeerActorTest.scala
@@ -1,13 +1,13 @@
 package coinffeine.peer.bitcoin
 
 import akka.actor.Props
-import akka.testkit.TestProbe
 import com.google.bitcoin.core.{FullPrunedBlockChain, PeerGroup}
 import com.google.bitcoin.store.MemoryFullPrunedBlockStore
 import org.scalatest.mock.MockitoSugar
 
 import coinffeine.common.akka.test.{MockSupervisedActor, AkkaSpec}
 import coinffeine.model.bitcoin.test.CoinffeineUnitTestNetwork
+import coinffeine.peer.api.event.EventChannelProbe
 
 class BitcoinPeerActorTest extends AkkaSpec with MockitoSugar {
 
@@ -23,9 +23,8 @@ class BitcoinPeerActorTest extends AkkaSpec with MockitoSugar {
 
   trait Fixture extends CoinffeineUnitTestNetwork.Component {
     val peerGroup = new PeerGroup(network)
-
     val blockchainActor, walletActor = new MockSupervisedActor()
-    val eventChannel = TestProbe().ref
+    val eventChannelProbe = EventChannelProbe()
     val blockchain = new FullPrunedBlockChain(network, new MemoryFullPrunedBlockStore(network, 1000))
     val actor = system.actorOf(Props(new BitcoinPeerActor(
       peerGroup, blockchainActor.props, walletActor.props, keyPairs = Seq.empty, blockchain, network)))
