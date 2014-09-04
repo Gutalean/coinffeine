c16416120f430d0f89a24867c9ac27478c55d232
Alvaro Polo <apoloval@gmail.com>
2014-09-04 18:05:24 +0200

    Allow dropping reference config items while saving
    A new boolean flag in `saveUserConfig()` can be used to indicate whether the
    config items that match those of reference config have to be dropped away. A
    config item is considered to match a reference one if both its key and value
    matches. In other words, if the user references a new value for a known config
    item, it shall be part of user configuration.

--- coinffeine-peer/src/test/scala/coinffeine/peer/config/FileConfigProviderTest.scala
+++ coinffeine-peer/src/test/scala/coinffeine/peer/config/FileConfigProviderTest.scala
@@ -1,6 +1,8 @@
 package coinffeine.peer.config
 
-import com.typesafe.config.ConfigFactory
+import scala.collection.JavaConversions._
+
+import com.typesafe.config.{ConfigValueFactory, ConfigFactory}
 import org.scalatest.BeforeAndAfterAll
 
 import coinffeine.common.test.UnitTest
@@ -27,5 +29,29 @@ class FileConfigProviderTest extends UnitTest with BeforeAndAfterAll {
 
   it should "refresh config after save user settings" in {
     provider.config.getInt("my.prop") shouldBe 7
+  }
+
+  it should "drop user config item while saving when it matches the reference one" in {
+    val configItem = provider.referenceConfig.root().entrySet().head
+    val cfg = ConfigFactory.empty().withValue(configItem.getKey, configItem.getValue)
+    provider.saveUserConfig(cfg)
+    provider.userConfig.hasPath(configItem.getKey) should be (false)
+  }
+
+  it should "not drop user config item while saving when its modified" in {
+    val configItem = provider.referenceConfig.root().entrySet().head
+    val replacedValue = ConfigValueFactory.fromAnyRef("__this is another value__")
+    val cfg = ConfigFactory.empty().withValue(configItem.getKey, replacedValue)
+    provider.saveUserConfig(cfg)
+    provider.userConfig.hasPath(configItem.getKey) should be (true)
+    provider.userConfig.getValue(configItem.getKey) should be (replacedValue)
+  }
+
+  it should "not drop user config item while saving when not instructed to drop" in {
+    val configItem = provider.referenceConfig.root().entrySet().head
+    val cfg = ConfigFactory.empty().withValue(configItem.getKey, configItem.getValue)
+    provider.saveUserConfig(cfg, dropReferenceValues = false)
+    provider.userConfig.hasPath(configItem.getKey) should be (true)
+    provider.userConfig.getValue(configItem.getKey) should be (configItem.getValue)
   }
 }
