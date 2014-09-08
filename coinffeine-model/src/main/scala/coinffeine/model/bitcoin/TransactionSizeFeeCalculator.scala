ac988957f373f234d56de9ba158d91610618cc9c
Alberto Gomez Toribio <gotoalberto@coinffeine.com>
2014-09-08 11:14:52 +0200

    Fix default fee amount

--- coinffeine-model/src/main/scala/coinffeine/model/bitcoin/TransactionSizeFeeCalculator.scala
+++ coinffeine-model/src/main/scala/coinffeine/model/bitcoin/TransactionSizeFeeCalculator.scala
@@ -4,8 +4,6 @@ import coinffeine.model.currency.Implicits._
 
 /** Computes fees based on the size of the transactions. */
 object TransactionSizeFeeCalculator extends BitcoinFeeCalculator {
-  private val DefaultTransactionBytes = 80
-  private val DefaultTransactionFee = 0.0001.BTC
 
-  override val defaultTransactionFee = DefaultTransactionFee * DefaultTransactionBytes
+  override val defaultTransactionFee = 0.0001.BTC
 }
