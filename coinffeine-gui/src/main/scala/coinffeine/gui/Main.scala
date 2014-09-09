918ab8b5c72892233b742d8c91c98c7b5c2c3e6a
Alvaro Polo <apoloval@gmail.com>
2014-09-09 18:04:40 +0200

    Make app to create the wallet after setup wizard
    And fill it with the newly created key pair.

--- coinffeine-gui/src/main/scala/coinffeine/gui/Main.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/Main.scala
@@ -11,9 +11,10 @@ import coinffeine.gui.application.operations.OperationsView
 import coinffeine.gui.application.{ApplicationProperties, ApplicationScene, NotificationManager}
 import coinffeine.gui.control.{ConnectionStatusWidget, WalletBalanceWidget}
 import coinffeine.gui.setup.SetupWizard
-import coinffeine.model.bitcoin.{IntegrationTestNetworkComponent, KeyPair}
+import coinffeine.model.bitcoin.{Wallet, IntegrationTestNetworkComponent, KeyPair}
 import coinffeine.model.currency.Currency.{Bitcoin, Euro}
 import coinffeine.peer.api.impl.ProductionCoinffeineApp
+import coinffeine.peer.bitcoin.DefaultBitcoinComponents
 
 object Main extends JFXApp
   with ProductionCoinffeineApp.Component with IntegrationTestNetworkComponent {
@@ -55,6 +56,8 @@ object Main extends JFXApp
     val address = keys.toAddress(network)
     val setupConfig = new SetupWizard(address.toString).run()
 
+    createWallet(keys)
+
     val bitcoinSettings = configProvider.bitcoinSettings
     configProvider.saveUserSettings(
       bitcoinSettings.copy(walletPrivateKey = keys.getPrivateKeyEncoded(network).toString))
@@ -64,5 +67,11 @@ object Main extends JFXApp
       configProvider.saveUserSettings(
         okPaySettings.copy(userAccount = access.walletId, seedToken = access.seedToken))
     }
+  }
+
+  private def createWallet(keys: KeyPair): Unit = {
+    val wallet = new Wallet(network)
+    wallet.addKey(keys)
+    wallet.saveToFile(DefaultBitcoinComponents.UserWalletFile)
   }
 }
