4e3ac872f18cf824037876cd6a29088392410d87
Alvaro Polo <apoloval@gmail.com>
2014-09-17 16:59:13 +0200

    Add a new kind of property: the `PropertyMap`
    This property models a thread-safe key-value store.

---
+++ coinffeine-model/src/main/scala/coinffeine/model/properties/MutablePropertyMap.scala
@@ -0,0 +1,25 @@
+package coinffeine.model.properties
+
+import scala.concurrent.ExecutionContext
+
+class MutablePropertyMap[K, V] extends PropertyMap[K, V] {
+
+  private val listeners = new PropertyListeners[OnEntryChangeHandler]
+  private var map: Map[K, V] = Map.empty
+
+  override def get(key: K): Option[V] = synchronized {
+    map.get(key)
+  }
+
+  override def onChange(handler: OnEntryChangeHandler)(implicit executor: ExecutionContext) = {
+    listeners.add(handler)
+  }
+
+  def set(key: K, value: V): Unit = synchronized {
+    val prevValue = map.get(key)
+    if (prevValue != Some(value)) {
+      map += key -> value
+      listeners.invoke(handler => handler(key, prevValue, value))
+    }
+  }
+}
---
+++ coinffeine-model/src/main/scala/coinffeine/model/properties/PropertyMap.scala
@@ -0,0 +1,41 @@
+package coinffeine.model.properties
+
+import scala.concurrent.ExecutionContext
+
+trait PropertyMap[K, V] {
+
+  /** A function aimed to process changes on a property entry.
+    *
+    * It has the form: f(key: K, oldValue: Option[V], newValue: V): Unit
+    */
+  type OnEntryChangeHandler = (K, Option[V], V) => Unit
+
+  /** A function aimed to process new values on a property entry.
+    *
+    * It has the form: f(key: K, newValue: V): Unit
+    */
+  type OnEntryNewValue = (K, V) => Unit
+
+  @throws[NoSuchElementException]
+  def apply(key: K): V = get(key).get
+
+  def get(key: K): Option[V]
+
+  /** Set a on-change handler which will be invoked when a property entry changes.
+    *
+    * @param handler    The handler to be invoked when some entry is changed
+    * @param executor   The executor used to invoke the handler
+    */
+  def onChange(handler: OnEntryChangeHandler)
+              (implicit executor: ExecutionContext): Cancellable
+
+  /** Set a on-change handler which will be invoked when a new entry value is set
+    *
+    * @param handler    The handler to be invoked when some entry value is set
+    * @param executor   The executor used to invoke the handler
+    */
+  def onNewValue(handler: OnEntryNewValue)
+                (implicit executor: ExecutionContext): Cancellable =
+    onChange((key, _, value) => handler(key, value))
+
+}
