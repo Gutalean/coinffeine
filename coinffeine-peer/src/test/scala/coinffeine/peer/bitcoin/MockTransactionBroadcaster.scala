8c0b5aa887633cb10af575d15d6680809fba239f
Sebastian Ortega <sortega@coinffeine.com>
2014-10-07 12:06:30 +0200

    Implement a broadcast strategy with retries
    It is encapsulated into a new actor, TransactionPublisher.

--- coinffeine-peer/src/test/scala/coinffeine/peer/bitcoin/MockTransactionBroadcaster.scala
+++ coinffeine-peer/src/test/scala/coinffeine/peer/bitcoin/MockTransactionBroadcaster.scala
@@ -9,13 +9,13 @@ import coinffeine.model.bitcoin.MutableTransaction
 
 class MockTransactionBroadcaster extends TransactionBroadcaster {
 
-  private var onBroadcast: MutableTransaction => MutableTransaction = recallTransaction
+  private var onBroadcast: MutableTransaction => Option[MutableTransaction] = recallTransaction
   private var _lastBroadcast: Option[MutableTransaction] = None
 
   override def broadcastTransaction(tx: MutableTransaction): ListenableFuture[MutableTransaction] = {
     val result = SettableFuture.create[MutableTransaction]()
     try {
-      result.set(onBroadcast(tx))
+      onBroadcast(tx).foreach(result.set)
     } catch {
       case NonFatal(e) => result.setException(e)
     }
@@ -24,20 +24,29 @@ class MockTransactionBroadcaster extends TransactionBroadcaster {
 
   def lastBroadcast: Option[MutableTransaction] = _lastBroadcast
 
-  def givenSuccessOnTransactionBroadcast(): Unit = {
+  def givenSuccessOnTransactionBroadcast(): Unit = synchronized {
     onBroadcast = recallTransaction
   }
 
-  def givenErrorOnTransactionBroadcast(error: Throwable): Unit = {
+  def givenErrorOnTransactionBroadcast(error: Throwable): Unit = synchronized {
     onBroadcast = throwError(error)
   }
 
-  private def recallTransaction(tx: MutableTransaction): MutableTransaction = {
-    _lastBroadcast = Some(tx)
-    tx
+  def givenTemporaryErrorOnTransactionBroadcast(error: Throwable): Unit = synchronized {
+    onBroadcast = tx => {
+      givenSuccessOnTransactionBroadcast()
+      throwError(error)(tx)
+    }
   }
 
-  private def throwError(error: Throwable)(tx: MutableTransaction): MutableTransaction = {
-    throw error
+  def givenNoResponseOnTransactionBroadcast(): Unit = synchronized {
+    onBroadcast = _ => None
   }
+
+  private def recallTransaction(tx: MutableTransaction) = synchronized {
+    _lastBroadcast = Some(tx)
+    Some(tx)
+  }
+
+  private def throwError(error: Throwable)(tx: MutableTransaction) = throw error
 }
