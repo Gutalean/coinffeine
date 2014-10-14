a7e113df0ed0ed65f469fe7736f265c2a64778b1
Sebastian Ortega <sortega@coinffeine.com>
2014-10-14 11:59:30 +0200

    Make some timeout use the akka time dilation
    This should avoid some intermittent false positives in tests

--- coinffeine-peer/src/test/scala/coinffeine/peer/exchange/handshake/ReRequestsHandshakeActorTest.scala
+++ coinffeine-peer/src/test/scala/coinffeine/peer/exchange/handshake/ReRequestsHandshakeActorTest.scala
@@ -2,14 +2,16 @@ package coinffeine.peer.exchange.handshake
 
 import scala.concurrent.duration._
 
+import akka.testkit._
+
 import coinffeine.peer.ProtocolConstants
 
 class ReRequestsHandshakeActorTest extends HandshakeActorTest("retries") {
 
   override def protocolConstants = ProtocolConstants(
     commitmentConfirmations = 1,
-    resubmitHandshakeMessagesTimeout = 500 millis,
-    refundSignatureAbortTimeout = 1 minute
+    resubmitHandshakeMessagesTimeout = 500.millis.dilated,
+    refundSignatureAbortTimeout = 1.minute.dilated
   )
 
   "The handshake actor" should "resubmit the counterpart peer handshake" in {
