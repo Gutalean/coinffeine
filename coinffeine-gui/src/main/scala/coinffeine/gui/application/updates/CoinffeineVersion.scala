7724793b4c5f66266586b853769593427097f2b1
Alvaro Polo <apoloval@gmail.com>
2014-10-23 16:11:40 +0200

    Fix check for updates
    * Fix invalid URL of version config
    * Fix wrong `CoinffeineVersion.isNewerThan()`

--- coinffeine-gui/src/main/scala/coinffeine/gui/application/updates/CoinffeineVersion.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/application/updates/CoinffeineVersion.scala
@@ -1,15 +1,15 @@
 package coinffeine.gui.application.updates
 
-case class CoinffeineVersion(major: Int, minor: Int, revision: Int, build: String) {
+case class CoinffeineVersion(major: Int, minor: Int, revision: Int, build: String = "") {
 
   private lazy val printableBuild = if (build.isEmpty) build else s"-$build"
 
   override def toString = s"v$major.$minor.$revision$printableBuild"
 
   def isNewerThan(other: CoinffeineVersion): Boolean =
-    this.major >= other.major &&
-    this.minor >= other.minor &&
-    this.revision >= other.revision
+    this.major > other.major ||
+    (this.major == other.major && this.minor > other.minor) ||
+    (this.major == other.major && this.minor == other.minor && this.revision > other.revision)
 }
 
 object CoinffeineVersion {
