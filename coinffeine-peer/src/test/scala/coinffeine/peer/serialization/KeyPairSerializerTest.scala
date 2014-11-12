eeadf83677465e233f51a0b40c06a1ac80b55473
Sebastian Ortega <sortega@coinffeine.com>
2014-11-12 15:08:32 +0100

    Extract base class for testing custom serializers

--- coinffeine-peer/src/test/scala/coinffeine/peer/serialization/KeyPairSerializerTest.scala
+++ coinffeine-peer/src/test/scala/coinffeine/peer/serialization/KeyPairSerializerTest.scala
@@ -1,47 +1,26 @@
 package coinffeine.peer.serialization
 
-import java.io.ByteArrayOutputStream
-
-import com.esotericsoftware.kryo.Kryo
-import com.esotericsoftware.kryo.io.{Input, Output}
-
-import coinffeine.common.test.UnitTest
 import coinffeine.model.bitcoin._
 import coinffeine.model.bitcoin.test.CoinffeineUnitTestNetwork
 
-class KeyPairSerializerTest extends UnitTest {
-
-  val kryo = new Kryo()
-  new KryoConfigurator().customize(kryo)
+class KeyPairSerializerTest extends SerializerTest {
 
   "Key pairs" should "support roundtrip Kryo serialization" in {
     val keyPair = new KeyPair()
-    KeyPairUtils.equals(keyPair, deserialize(serialize(keyPair))) shouldBe true
+    KeyPairUtils.equals(keyPair, keyPairSerializationRoundtrip(keyPair)) shouldBe true
   }
 
   "Public keys" should "support roundtrip Kryo serialization" in {
     val publicKey = new KeyPair().publicKey
-    KeyPairUtils.equals(publicKey, deserialize(serialize(publicKey))) shouldBe true
+    KeyPairUtils.equals(publicKey, keyPairSerializationRoundtrip(publicKey)) shouldBe true
   }
 
   "Deterministic keys" should "support roundtrip Kryo serialization" in {
     val wallet = new Wallet(CoinffeineUnitTestNetwork)
     val keyPair = wallet.freshReceiveKey()
     keyPair.canSign shouldBe true
-    val deserializedKeyPair = deserialize(serialize(keyPair))
+    val deserializedKeyPair = keyPairSerializationRoundtrip(keyPair)
     KeyPairUtils.equals(keyPair, deserializedKeyPair) shouldBe true
     deserializedKeyPair.canSign shouldBe true
-  }
-
-  def serialize(keyPair: KeyPair): Array[Byte] = {
-    val stream = new ByteArrayOutputStream()
-    val output = new Output(stream)
-    kryo.writeObject(output, keyPair)
-    output.flush()
-    stream.toByteArray
-  }
-
-  def deserialize(bytes: Array[Byte]): KeyPair = {
-    kryo.readObject(new Input(bytes), classOf[KeyPair])
   }
 }
