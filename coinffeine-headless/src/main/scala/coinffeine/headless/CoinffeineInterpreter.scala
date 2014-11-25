4d6e5a7b37ce424154fa469ac443368495fecdd4
Sebastian Ortega <sortega@coinffeine.com>
2014-11-25 17:40:11 +0100

    Add a basic status command
    Just balance information for now

--- coinffeine-headless/src/main/scala/coinffeine/headless/CoinffeineInterpreter.scala
+++ coinffeine-headless/src/main/scala/coinffeine/headless/CoinffeineInterpreter.scala
@@ -1,12 +1,13 @@
 package coinffeine.headless
 
+import coinffeine.headless.commands.StatusCommand
 import coinffeine.headless.prompt.ConnectionStatusPrompt
 import coinffeine.headless.shell.{Command, Shell}
 import coinffeine.peer.api.CoinffeineApp
 
 class CoinffeineInterpreter(app: CoinffeineApp)
-  extends Shell(new ConnectionStatusPrompt(app), CoinffeineInterpreter.Commands)
+  extends Shell(new ConnectionStatusPrompt(app), CoinffeineInterpreter.commands(app))
 
 object CoinffeineInterpreter {
-  private val Commands = Seq.empty[Command]
+  private def commands(app: CoinffeineApp): Seq[Command] = Seq(new StatusCommand(app))
 }
