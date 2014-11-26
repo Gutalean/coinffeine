2a855947d5d92a442a6b45751970407621995b8c
Sebastian Ortega <sortega@coinffeine.com>
2014-11-26 17:01:37 +0100

    Implement OrderId.parse

---
+++ coinffeine-model/src/test/scala/coinffeine/model/market/OrderIdTest.scala
@@ -0,0 +1,15 @@
+package coinffeine.model.market
+
+import coinffeine.common.test.UnitTest
+
+class OrderIdTest extends UnitTest {
+
+  "An order id" should "be parsed from an UUID" in {
+    val id = OrderId.random()
+    OrderId.parse(id.value) shouldBe Some(id)
+  }
+
+  it should "not be parsed from invalid strings" in {
+    OrderId.parse("invalid") shouldBe 'empty
+  }
+}
