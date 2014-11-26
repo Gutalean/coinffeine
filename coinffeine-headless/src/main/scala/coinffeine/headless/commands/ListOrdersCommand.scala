edefcd12751a13ed156d8ef8892c78cf57928239
Sebastian Ortega <sortega@coinffeine.com>
2014-11-26 10:17:58 +0100

    Add the list-orders command

---
+++ coinffeine-headless/src/main/scala/coinffeine/headless/commands/ListOrdersCommand.scala
@@ -0,0 +1,23 @@
+package coinffeine.headless.commands
+
+import java.io.PrintWriter
+
+import coinffeine.headless.shell.Command
+import coinffeine.model.market._
+import coinffeine.model.properties.PropertyMap
+
+class ListOrdersCommand(ordersProperty: PropertyMap[OrderId, AnyCurrencyOrder]) extends Command {
+  override val keyword = "list-orders"
+
+  override def apply(output: PrintWriter, args: String): Unit = {
+    val currentOrders = ordersProperty.content.map(_._2).toList.sortBy(_.id.value)
+    if (currentOrders.isEmpty) output.println("No orders")
+    else printOrders(output, currentOrders)
+  }
+
+  def printOrders(output: PrintWriter, orders: List[AnyCurrencyOrder]): Unit = {
+    orders.foreach { order =>
+      output.format("%s\t%s\t%s\t%s%n", order.id.value, order.status, order.amount, order.price)
+    }
+  }
+}
---
+++ coinffeine-headless/src/test/scala/coinffeine/headless/commands/ListOrdersCommandTest.scala
@@ -0,0 +1,32 @@
+package coinffeine.headless.commands
+
+import coinffeine.model.market._
+import coinffeine.model.currency._
+import coinffeine.model.properties.MutablePropertyMap
+
+class ListOrdersCommandTest extends CommandTest {
+
+  val orderMap = new MutablePropertyMap[OrderId, AnyCurrencyOrder]()
+  val command = new ListOrdersCommand(orderMap)
+
+  "The list orders command" should "print a message when no orders exist" in {
+    executeCommand(command) should include("No orders")
+  }
+
+  it should "show order id, state and relevant amounts of existing orders" in {
+    val order = Order(Bid, 10.BTC, Price(500.EUR))
+    orderMap.set(order.id, order)
+    executeCommand(command) should (
+      include(order.id.value) and include(order.status.toString) and
+        include(order.amount.toString) and include(order.price.toString))
+  }
+
+  it should "sort orders by the first field, the id" in {
+    for (index <- 1 to 10) {
+      val order = Order(Bid, index.BTC, Price((500 - index).EUR))
+      orderMap.set(order.id, order)
+    }
+    val outputLines = executeCommand(command).lines.toList
+    outputLines.sorted shouldBe outputLines
+  }
+}
