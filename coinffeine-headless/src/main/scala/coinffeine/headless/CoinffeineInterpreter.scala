fe9e6a8f0edd094f66921724c7fc5d99cc0da087
Sebastian Ortega <sortega@coinffeine.com>
2014-11-26 15:56:44 +0100

    Enable help, bid and ask commands

--- coinffeine-headless/src/main/scala/coinffeine/headless/CoinffeineInterpreter.scala
+++ coinffeine-headless/src/main/scala/coinffeine/headless/CoinffeineInterpreter.scala
@@ -1,16 +1,22 @@
 package coinffeine.headless
 
-import coinffeine.headless.commands.{ListOrdersCommand, StatusCommand}
+import coinffeine.headless.commands.{HelpCommand, ListOrdersCommand, OpenOrderCommand, StatusCommand}
 import coinffeine.headless.prompt.ConnectionStatusPrompt
 import coinffeine.headless.shell.{Command, Shell}
+import coinffeine.model.market.{Ask, Bid}
 import coinffeine.peer.api.CoinffeineApp
 
 class CoinffeineInterpreter(app: CoinffeineApp)
   extends Shell(new ConnectionStatusPrompt(app), CoinffeineInterpreter.commands(app))
 
 object CoinffeineInterpreter {
-  private def commands(app: CoinffeineApp): Seq[Command] = Seq(
-    new StatusCommand(app),
-    new ListOrdersCommand(app.network.orders)
-  )
+  private def commands(app: CoinffeineApp): Seq[Command] = {
+    val actions = Seq(
+      new StatusCommand(app),
+      new ListOrdersCommand(app.network.orders),
+      new OpenOrderCommand(Bid, app.network),
+      new OpenOrderCommand(Ask, app.network)
+    )
+    actions :+ new HelpCommand(actions)
+  }
 }
