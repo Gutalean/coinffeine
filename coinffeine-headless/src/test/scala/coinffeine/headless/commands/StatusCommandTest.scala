e2615fe865a8c21f6237aea56b7714ed6e8410e6
Sebastian Ortega <sortega@coinffeine.com>
2014-11-26 10:17:24 +0100

    Introduce a base class for Command tests

--- coinffeine-headless/src/test/scala/coinffeine/headless/commands/StatusCommandTest.scala
+++ coinffeine-headless/src/test/scala/coinffeine/headless/commands/StatusCommandTest.scala
@@ -1,46 +1,39 @@
 package coinffeine.headless.commands
 
-import java.io.{PrintWriter, StringWriter}
 import scala.concurrent.Future
 import scala.concurrent.duration.FiniteDuration
 
-import coinffeine.common.test.UnitTest
 import coinffeine.model.bitcoin.{Address, Hash}
 import coinffeine.model.currency._
 import coinffeine.model.payment.PaymentProcessor.AccountId
 import coinffeine.model.properties.{MutableProperty, MutablePropertyMap, PropertyMap}
 import coinffeine.peer.api._
 
-class StatusCommandTest extends UnitTest {
+class StatusCommandTest extends CommandTest {
 
   "The status command" should "print the available and blocked fiat" in new Fixture {
-    executeCommand() should include("FIAT: --")
+    commandOutput() should include("FIAT: --")
     app.fiatBalance.set(Some(CoinffeinePaymentProcessor.Balance(0.05.EUR)))
-    executeCommand() should include("FIAT: 0.05 EUR")
+    commandOutput() should include("FIAT: 0.05 EUR")
     app.fiatBalance.set(Some(CoinffeinePaymentProcessor.Balance(123.45.EUR, 20.3.EUR)))
-    executeCommand() should include("FIAT: 123.45 EUR (20.30 EUR blocked)")
+    commandOutput() should include("FIAT: 123.45 EUR (20.30 EUR blocked)")
   }
 
   it should "print the bitcoin wallet balances" in new Fixture {
-    executeCommand() should include("BTC: --")
+    commandOutput() should include("BTC: --")
     app.btcBalance.set(Some(BitcoinBalance(
       estimated = 10.BTC, available = 8.BTC, minOutput = Some(0.01.BTC))))
-    executeCommand() should include("BTC: 10.00000000 BTC estimated, 8.00000000 BTC available")
+    commandOutput() should include("BTC: 10.00000000 BTC estimated, 8.00000000 BTC available")
     app.btcBalance.set(Some(BitcoinBalance(
       estimated = 10.BTC, available = 8.BTC, blocked = 2.BTC, minOutput = Some(0.01.BTC))))
-    executeCommand() should include(
+    commandOutput() should include(
       "BTC: 10.00000000 BTC estimated, 8.00000000 BTC available (2.00000000 BTC blocked)")
   }
 
   trait Fixture {
     val app = new MockCoinffeineApp
     val command = new StatusCommand(app)
-
-    def executeCommand(): String = {
-      val outputWriter = new StringWriter()
-      command.apply(new PrintWriter(outputWriter), "")
-      outputWriter.toString
-    }
+    def commandOutput(): String = executeCommand(command)
   }
 
   class MockCoinffeineApp extends CoinffeineApp {
