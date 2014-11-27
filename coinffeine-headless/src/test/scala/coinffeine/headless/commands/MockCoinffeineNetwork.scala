e2a470820b0d506cd3da580117e2f587fe6457fb
Sebastian Ortega <sortega@coinffeine.com>
2014-11-27 11:08:56 +0100

    Extract code from CancelOrderCommand
    Extract order id validation from the production code and a mock class
    from its test for reuse. ShowOrderDetailsCommand shares that behaviour
    and will benefit from this.

---
+++ coinffeine-headless/src/test/scala/coinffeine/headless/commands/MockCoinffeineNetwork.scala
@@ -0,0 +1,20 @@
+package coinffeine.headless.commands
+
+import coinffeine.model.currency.Euro
+import coinffeine.model.market._
+import coinffeine.model.properties.MutablePropertyMap
+
+class MockCoinffeineNetwork extends DummyCoinffeineNetwork {
+
+  override val orders = new MutablePropertyMap[OrderId, AnyCurrencyOrder]
+
+  def givenOrderExists(order: Order[Euro.type]): Unit = {
+    orders.set(order.id, order)
+  }
+
+  private var _cancellations = Seq.empty[OrderId]
+
+  def cancellations: Seq[OrderId] = _cancellations
+
+  override def cancelOrder(order: OrderId): Unit = synchronized { _cancellations :+= order }
+}
---
+++ coinffeine-headless/src/test/scala/coinffeine/headless/commands/OrderIdValidationTest.scala
@@ -0,0 +1,34 @@
+package coinffeine.headless.commands
+
+import scalaz.Scalaz
+
+import coinffeine.common.test.UnitTest
+import coinffeine.model.market._
+import coinffeine.model.currency._
+
+class OrderIdValidationTest extends UnitTest {
+  import Scalaz._
+
+  val network = new MockCoinffeineNetwork
+  val validator = new OrderIdValidation(network)
+
+  "Order id validation" should "fail for empty inputs" in {
+    validator.requireExistingOrderId("") shouldBe "missing order id".fail
+  }
+
+  it should "fail for malformed order ids" in {
+    validator.requireExistingOrderId("malformed") shouldBe "invalid order id: 'malformed'".fail
+  }
+
+  it should "fail for ids of non existing orders" in {
+    val input = OrderId.random().value
+    validator.requireExistingOrderId(input) shouldBe "order not found".fail
+  }
+
+  it should "accept well-formed ids of existing orders" in {
+    val order = Order(Bid, 1.BTC, Price(1.EUR))
+    network.givenOrderExists(order)
+    val input = order.id.value
+    validator.requireExistingOrderId(input) shouldBe order.id.success
+  }
+}
