54c132306133851fea9683f45b7b171595b1e65f
Sebastian Ortega <sortega@coinffeine.com>
2014-12-10 14:57:26 +0100

    Reduce AskPattern timeout error verbosity
    And add its missing unit test

---
+++ coinffeine-common-akka/src/test/java/coinffeine/common/akka/AskPatternTest.scala
@@ -0,0 +1,66 @@
+package coinffeine.common.akka
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+import akka.testkit._
+import akka.util.Timeout
+
+import coinffeine.common.akka.test.AkkaSpec
+
+class AskPatternTest extends AkkaSpec {
+
+  import system.dispatcher
+
+  case object Request
+  case class Response(payload: String)
+  case class Error(code: Int)
+
+  val testTimeout = 200.millis.dilated
+  implicit val timeout = Timeout(testTimeout)
+
+  "The improved ask pattern" should "report timeouts" in new Fixture {
+    val ex = expectFailure[RuntimeException](AskPattern(
+      to = probe.ref,
+      request = Request,
+      errorMessage = "cannot perform request"
+    ).withReply[Response])
+    ex.getMessage shouldBe "cannot perform request: timeout waiting for response"
+  }
+
+  it should "return a response of the expected type" in new Fixture {
+    val future = AskPattern(
+      to = probe.ref,
+      request = Request,
+      errorMessage = "cannot perform request"
+    ).withReply[Response]
+
+    probe.expectMsg(Request)
+    probe.reply(Response("OK"))
+
+    future.futureValue(testTimeout * 2) shouldBe Response("OK")
+  }
+
+  it should "extract an error from a failed request" in new Fixture {
+    val future = AskPattern(
+      to = probe.ref,
+      request = Request,
+      errorMessage = "cannot perform request"
+    ).withReplyOrError[Response, Error](error => new Exception("code " + error.code))
+
+    probe.expectMsg(Request)
+    probe.reply(Error(501))
+
+    expectFailure[Exception](future).getCause.getMessage should include("code 501")
+  }
+
+  trait Fixture {
+    val probe = TestProbe()
+
+    def expectFailure[E: Manifest](f: Future[_]): E = {
+      the [E] thrownBy {
+        f.futureValue(testTimeout * 2)
+      }
+    }
+  }
+}
