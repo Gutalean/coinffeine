a25c53f0f445d006256e5d7fd1c7a58dbfb18402
Sebastian Ortega <sortega@coinffeine.com>
2014-12-12 14:14:53 +0100

    Fix port forwarding network configuration

--- coinffeine-protocol/src/main/scala/coinffeine/protocol/gateway/p2p/DefaultPeerFactory.scala
+++ coinffeine-protocol/src/main/scala/coinffeine/protocol/gateway/p2p/DefaultPeerFactory.scala
@@ -3,6 +3,7 @@ package coinffeine.protocol.gateway.p2p
 import java.net.InetSocketAddress
 import scala.concurrent.{ExecutionContext, Future}
 
+import net.tomp2p.connection.Bindings
 import net.tomp2p.futures.FutureDiscover
 import net.tomp2p.p2p.{Peer, PeerMaker}
 import net.tomp2p.peers.PeerAddress
@@ -21,9 +22,11 @@ private class DefaultPeerFactory(broker: NetworkEndpoint,
                                  forwardedPort: Option[Int] = None)
                                 (implicit val ec: ExecutionContext) extends PeerFactory {
 
-  override def bindPeer(id: PeerId, bindingsBuilder: BindingsBuilder) = Future {
+  override def bindPeer(id: PeerId, bindingsBuilder: BindingsBuilder) = for {
+    bindings <- buildBindings(id, bindingsBuilder)
+  } yield {
     val builder = new PeerMaker(Number160Util.fromPeerId(id))
-      .setBindings(bindingsBuilder.defaultBindings())
+      .setBindings(bindings)
       .setPorts(localPort)
     forwardedPort.foreach(builder.setPorts)
     val peer = builder.makeAndListen()
@@ -31,19 +34,30 @@ private class DefaultPeerFactory(broker: NetworkEndpoint,
     peer
   }
 
+  private def buildBindings(id: PeerId, bindingsBuilder: BindingsBuilder): Future[Bindings] =
+    forwardedPort.map { port =>
+      new ExternalIpProbe().detect(id, broker).map { ip =>
+        bindingsBuilder.bindToAddress(new InetSocketAddress(ip, port))
+      }
+    }.getOrElse(Future.successful(bindingsBuilder.defaultBindings()))
+
   override def bootstrapNode(id: PeerId, peer: Peer): Future[PeerId] = for {
     brokerAddress <- broker.resolveAsync()
-    brokerPeerAddress <- connectWithBroker(peer, brokerAddress)
-    bootstrap <- peer.bootstrap().setPeerAddress(brokerPeerAddress).start()
+    _ <- if (forwardedPort.isEmpty) discoverNetworkConnectivity(peer, brokerAddress)
+         else Future.successful {}
+    bootstrap <- peer.bootstrap()
+      .setInetAddress(brokerAddress.getAddress)
+      .setPorts(brokerAddress.getPort).start()
     _ <- if (isBehindFirewall(peer)) Future.successful(AddressDHT.removeOwnAddress(peer))
          else AddressDHT.storeOwnAddress(peer)
   } yield {
     logger.info("Successfully connected with {} as {} listening at {}",
       broker, id, peer.getPeerAddress)
-    Number160Util.toPeerId(brokerPeerAddress)
+    Number160Util.toPeerId(bootstrap.getBootstrapTo.iterator.next())
   }
 
-  private def connectWithBroker(peer: Peer, brokerAddress: InetSocketAddress): Future[PeerAddress] = {
+  private def discoverNetworkConnectivity(
+      peer: Peer, brokerAddress: InetSocketAddress): Future[PeerAddress] = {
     val discovery = peer.discover()
       .setInetAddress(brokerAddress.getAddress)
       .setPorts(brokerAddress.getPort)
