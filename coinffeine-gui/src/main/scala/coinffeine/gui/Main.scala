10339f6f6346c7f7814e6e217b4be56dbd8ee4bc
Alvaro Polo <apoloval@gmail.com>
2014-12-16 15:43:31 +0100

    Prevent abrupt exiting when setup wizard is cancelled
    Instead of letting the exception to be thrown behind main program, capture it
    and log a message.

--- coinffeine-gui/src/main/scala/coinffeine/gui/Main.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/Main.scala
@@ -4,17 +4,22 @@ import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 import scalafx.application.JFXApp
 
+import com.typesafe.scalalogging.LazyLogging
 import org.controlsfx.dialog.{DialogStyle, Dialogs}
 
 import coinffeine.gui.application.launcher.AppLauncher
+import coinffeine.gui.wizard.Wizard
 import coinffeine.peer.api.impl.ProductionCoinffeineComponent
 
-object Main extends JFXApp with ProductionCoinffeineComponent with AppLauncher {
+object Main extends JFXApp with ProductionCoinffeineComponent with AppLauncher with LazyLogging {
 
   private val issueReportingResource = "https://github.com/coinffeine/coinffeine/issues"
 
   launchApp() match {
     case Success(s) => stage = s
+    case Failure(_: Wizard.CancelledByUser) =>
+      logger.info("Exiting after wizard cancellation.")
+      System.exit(0)
     case Failure(e) =>
       Dialogs.create()
         .message("An unexpected error was thrown while starting Coinffeine app. " +
