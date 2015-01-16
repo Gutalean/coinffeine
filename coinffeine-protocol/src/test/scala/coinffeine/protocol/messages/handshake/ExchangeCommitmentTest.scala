c24616e7a5a0513508b79ae860da11f03e9a19a7
Sebastian Ortega <sortega@coinffeine.com>
2015-01-16 10:51:24 +0100

    Add missing test case to ExchangeCommitment

--- coinffeine-protocol/src/test/scala/coinffeine/protocol/messages/handshake/ExchangeCommitmentTest.scala
+++ coinffeine-protocol/src/test/scala/coinffeine/protocol/messages/handshake/ExchangeCommitmentTest.scala
@@ -1,19 +1,23 @@
 package coinffeine.protocol.messages.handshake
 
 import coinffeine.common.test.UnitTest
+import coinffeine.model.bitcoin._
 import coinffeine.model.bitcoin.test.CoinffeineUnitTestNetwork
-import coinffeine.model.bitcoin.{KeyPair, MutableTransaction, ImmutableTransaction}
 import coinffeine.model.exchange.ExchangeId
 
 class ExchangeCommitmentTest extends UnitTest {
 
+  val tx = ImmutableTransaction(new MutableTransaction(CoinffeineUnitTestNetwork))
+  val keyPair = new KeyPair()
+
   "An exchange commitment" should "prevent private key publication" in {
     an [IllegalArgumentException] shouldBe thrownBy {
-      ExchangeCommitment(
-        ExchangeId("id"),
-        new KeyPair(),
-        ImmutableTransaction(new MutableTransaction(CoinffeineUnitTestNetwork))
-      )
+      ExchangeCommitment(ExchangeId.random(), keyPair, tx)
     }
+  }
+
+  it should "have a compact string representation" in {
+    ExchangeCommitment(ExchangeId.random(), keyPair.publicKey, tx).toString should
+      fullyMatch regex "ExchangeCommitment(.*, key=.*, tx=.*)"
   }
 }
