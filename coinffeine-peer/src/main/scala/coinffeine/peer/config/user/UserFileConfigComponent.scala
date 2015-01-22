1a086bcb17a57f3fb099723b44c304dca95e945e
Sebastian Ortega <sortega@coinffeine.com>
2015-01-22 11:47:59 +0100

    Avoid creating ~/.coinffeine unnecessarily
    Move the creation responsibility away from LocalAppDataDir. Remove such
    class after that as it is empty of content.

--- coinffeine-peer/src/main/scala/coinffeine/peer/config/user/UserFileConfigComponent.scala
+++ coinffeine-peer/src/main/scala/coinffeine/peer/config/user/UserFileConfigComponent.scala
@@ -2,6 +2,7 @@ package coinffeine.peer.config.user
 
 import java.io.File
 
+import coinffeine.common.Platform
 import coinffeine.peer.config.{ConfigComponent, ConfigProvider}
 
 trait UserFileConfigComponent extends ConfigComponent {
@@ -11,8 +12,8 @@ trait UserFileConfigComponent extends ConfigComponent {
   lazy val configProvider: ConfigProvider = new UserFileConfigProvider(dataPath)
 
   def dataPath: File = {
-    val defaultPath = LocalAppDataDir().toAbsolutePath.toFile
-    commandLineArgs match {
+    val defaultPath = Platform.detect().userSettingsPath().toAbsolutePath.toFile
+    val path = commandLineArgs match {
       case Seq() => defaultPath
       case Seq("--data-path", dataPath) => new File(dataPath)
       case _ =>
@@ -25,6 +26,19 @@ trait UserFileConfigComponent extends ConfigComponent {
            """.stripMargin)
         System.exit(-1)
         throw new IllegalStateException("Should not reach this line")
+    }
+    ensureDirExists(path)
+    path
+  }
+
+  private def ensureDirExists(dir: File): Unit = {
+    if (!dir.exists()) {
+      if (!dir.mkdir()) {
+        throw new IllegalStateException(s"cannot create local app data directory in $dir")
+      }
+    } else if (!dir.isDirectory) {
+      throw new IllegalStateException(
+        s"there is a file in $dir where local app data directory was expected")
     }
   }
 }
