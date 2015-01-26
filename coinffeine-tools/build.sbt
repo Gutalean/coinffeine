999dabf3b3a9fa599782e4d30cc0b6fd60c27ccb
Sebastian Ortega <sortega@coinffeine.com>
2015-01-26 12:30:07 +0100

    Add new module coinffeine-tools

---
+++ coinffeine-tools/build.sbt
@@ -0,0 +1,1 @@
+name := "coinffeine-tools"
--- project/Build.scala
+++ project/Build.scala
@@ -10,16 +10,29 @@ import scoverage.ScoverageSbtPlugin
 
 object Build extends sbt.Build {
 
-  lazy val root = (Project(id = "coinffeine", base = file("."))
-    aggregate(headless, peer, protocol, model, overlay, common, commonAkka, commonTest, gui, server, okpaymock)
+  def subModule(id: String): Project = Project(id = id, base = file(s"coinffeine-$id"))
+
+  lazy val root = Project(id = "coinffeine", base = file(".")).aggregate(
+    common,
+    commonAkka,
+    commonTest,
+    gui,
+    headless,
+    model,
+    okpaymock,
+    overlay,
+    peer,
+    protocol,
+    server,
+    tools
   )
 
-  lazy val server = (Project(id = "server", base = file("coinffeine-server"))
+  lazy val server = (subModule("server")
     settings(Assembly.settings("coinffeine.server.main.Main"): _*)
     dependsOn(peer % "compile->compile;test->test", commonTest % "test->compile")
   )
 
-  lazy val peer = (Project(id = "peer", base = file("coinffeine-peer"))
+  lazy val peer = (subModule("peer")
     settings(scalaxbSettings: _*)
     settings(
       sourceGenerators in Compile <+= scalaxb in Compile,
@@ -28,62 +41,59 @@ object Build extends sbt.Build {
       async in (Compile, scalaxb) := true
     )
     dependsOn(
-      model % "compile->compile;test->test",
-      protocol % "compile->compile;test->test",
       commonAkka % "compile->compile;test->test",
-      commonTest % "test->compile"
+      commonTest % "test->compile",
+      model % "compile->compile;test->test",
+      protocol % "compile->compile;test->test"
     )
   )
 
-  lazy val protocol = (Project(id = "protocol", base = file("coinffeine-protocol"))
+  lazy val protocol = (subModule("protocol")
     settings(PB.protobufSettings: _*)
     dependsOn(
       common,
-      model % "compile->compile;test->test",
-      overlay % "compile->compile;test->test",
       commonAkka % "compile->compile;test->test",
-      commonTest % "test->compile"
+      commonTest % "test->compile",
+      model % "compile->compile;test->test",
+      overlay % "compile->compile;test->test"
     )
   )
 
-  lazy val overlay = (Project(id = "overlay", base = file("coinffeine-overlay"))
+  lazy val overlay = (subModule("overlay")
     settings(PB.protobufSettings: _*)
     dependsOn(common, commonAkka % "compile->compile;test->test", commonTest % "test->compile")
   )
 
-  lazy val model = (Project(id = "model", base = file("coinffeine-model"))
+  lazy val model = (subModule("model")
     dependsOn(commonTest % "test->compile")
   )
 
-  lazy val common = (Project(id = "common", base = file("coinffeine-common"))
-    dependsOn commonTest
-  )
+  lazy val common = subModule("common").dependsOn(commonTest)
 
-  lazy val commonAkka = (Project(id = "common-akka", base = file("coinffeine-common-akka"))
-    dependsOn(commonTest % "compile->compile;test->test")
-  )
+  lazy val commonAkka = subModule("common-akka").dependsOn(commonTest % "compile->compile;test->test")
 
-  lazy val commonTest = (Project(id = "common-test", base = file("coinffeine-common-test"))
-    settings(PB.protobufSettings: _*)
-  )
+  lazy val commonTest = subModule("common-test").settings(PB.protobufSettings: _*)
 
-  lazy val headless = (Project(id = "headless", base = file("coinffeine-headless"))
+  lazy val headless = (subModule("headless")
     settings(Assembly.settings("coinffeine.headless.Main"): _*)
     dependsOn(peer % "compile->compile;test->test", commonTest)
   )
 
-  lazy val gui = (Project(id = "gui", base = file("coinffeine-gui"))
+  lazy val gui = (subModule("gui")
     configs IntegrationTest
     settings(Defaults.itSettings: _*)
     dependsOn(peer % "compile->compile;test,it->test", commonTest)
   )
 
-  lazy val test = (Project(id = "test", base = file("coinffeine-test"))
-    dependsOn(peer % "compile->compile;test->test", server, okpaymock,
-      commonAkka % "compile->compile;test->test", commonTest % "compile->compile;test->compile")
+  lazy val test = subModule("test").dependsOn(
+    commonAkka % "compile->compile;test->test",
+    commonTest % "compile->compile;test->compile",
+    okpaymock,
+    peer % "compile->compile;test->test",
+    server
   )
 
-  lazy val okpaymock = (Project(id = "okpaymock", base = file("coinffeine-okpaymock"))
+  lazy val okpaymock = (subModule("okpaymock")
     settings(CxfWsdl2JavaPlugin.cxf.settings: _*)
     settings(Assembly.settings("coinffeine.okpaymock.main.Main"): _*)
     settings(
@@ -96,19 +106,24 @@ object Build extends sbt.Build {
       )
     )
     dependsOn(
-      model,
-      peer,
       commonAkka % "compile->compile;test->test",
-      commonTest % "compile->compile;test->compile"
+      commonTest % "compile->compile;test->compile",
+      model,
+      peer
     )
   )
 
-  lazy val benchmark = (Project(id = "benchmark", base = file("coinffeine-benchmark"))
+  lazy val benchmark = (subModule("benchmark")
     dependsOn(
-      peer % "compile->compile;test->test",
       commonAkka % "compile->compile;test->test",
-      commonTest % "compile->compile;test->compile"
+      commonTest % "compile->compile;test->compile",
+      peer % "compile->compile;test->test"
     )
     enablePlugins GatlingPlugin
+  )
+
+  lazy val tools = subModule("tools").dependsOn(
+    commonTest,
+    model % "compile->compile;test->test"
   )
 }
