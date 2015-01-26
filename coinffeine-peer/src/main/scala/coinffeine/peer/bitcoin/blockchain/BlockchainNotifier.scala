430cd545f2bcf3485f5b50f4f854ae1daf91791f
Sebastian Ortega <sortega@coinffeine.com>
2015-01-26 17:26:04 +0100

    Add more detailed logging to BlockchainNotifier

--- coinffeine-peer/src/main/scala/coinffeine/peer/bitcoin/blockchain/BlockchainNotifier.scala
+++ coinffeine-peer/src/main/scala/coinffeine/peer/bitcoin/blockchain/BlockchainNotifier.scala
@@ -3,14 +3,14 @@ package coinffeine.peer.bitcoin.blockchain
 import java.util
 import scala.collection.JavaConversions._
 
-import com.typesafe.scalalogging.LazyLogging
+import com.typesafe.scalalogging.StrictLogging
 import org.bitcoinj.core.AbstractBlockChain.NewBlockType
 import org.bitcoinj.core._
 
 import coinffeine.model.bitcoin.{Hash, ImmutableTransaction}
 
 private[blockchain] class BlockchainNotifier(initialHeight: Int)
-  extends AbstractBlockChainListener with LazyLogging {
+  extends AbstractBlockChainListener with StrictLogging {
   import BlockchainNotifier._
 
   private case class BlockIdentity(hash: Hash, height: Int)
@@ -40,35 +40,41 @@ private[blockchain] class BlockchainNotifier(initialHeight: Int)
   def watchTransactionConfirmation(tx: Hash,
                                    confirmations: Int,
                                    listener: ConfirmationListener): Unit = synchronized {
+    logger.debug(s"Subscribing to $tx, waiting for $confirmations confirmations")
     confirmationSubscriptions += tx -> ConfirmationSubscription(tx, listener, confirmations)
   }
 
   def watchHeight(height: Long, listener: HeightListener): Unit = synchronized {
     if (height <= currentHeight) {
+      logger.debug(s"Already at $currentHeight height no need to watch for $height")
       listener.heightReached(currentHeight)
     } else {
+      logger.debug(s"Watching height until block $height")
       heightSubscriptions += HeightSubscription(height, listener)
     }
   }
 
   def watchOutput(output: TransactionOutPoint, listener: OutputListener): Unit = synchronized {
+    logger.debug(s"Watching $output output")
     val updatedListeners = outputSubscriptions(output) + listener
     outputSubscriptions += output -> updatedListeners
   }
 
   override def notifyNewBestBlock(block: StoredBlock): Unit = synchronized {
     currentHeight = block.getHeight
+    logger.trace(s"New block ${block.getHeader.getHash} at height $currentHeight arrived")
     notifyConfirmedTransactions()
     notifyHeight()
   }
 
   override def reorganize(splitPoint: StoredBlock, oldBlocks: util.List[StoredBlock],
                           newBlocks: util.List[StoredBlock]): Unit = synchronized {
+    logger.warn(s"Blockchain reorganization from height ${splitPoint.getHeight}")
     val seenTxs = confirmationSubscriptions.values.filter(_.foundInBlock.isDefined)
     val rejectedTransactions = seenTxs.filterNot(tx =>
       newBlocks.toSeq.exists(block => block.getHeader.getHash == tx.foundInBlock.get.hash))
     rejectedTransactions.foreach { subscription =>
-      logger.info("tx {} is lost in blockchain reorganization; reporting to the observer",
+      logger.warn("Tx {} is lost in blockchain reorganization; reporting to the observer",
         subscription.txHash)
       confirmationSubscriptions -= subscription.txHash
       subscription.listener.rejected(subscription.txHash)
@@ -97,6 +103,7 @@ private[blockchain] class BlockchainNotifier(initialHeight: Int)
   }
 
   private def updateConfirmationSubscriptions(tx: Hash, block: StoredBlock): Unit = {
+    logger.debug(s"tx $tx found in block ${block.getHeight}")
     confirmationSubscriptions.get(tx).foreach { subscription =>
       logger.info(s"tx $tx found in block ${block.getHeight}: " +
         s"waiting for ${subscription.requiredConfirmations} confirmations")
@@ -106,17 +113,30 @@ private[blockchain] class BlockchainNotifier(initialHeight: Int)
   }
 
   private def notifySpentOutputs(tx: Transaction): Unit = {
+    if (outputSubscriptions.nonEmpty) {
+      logger.trace(s"Checking for ${tx.getHashAsString} spending watched outputs. TX=$tx")
+    }
     for (input <- tx.getInputs) {
-      outputSubscriptions(input.getOutpoint).foreach { subscription =>
+      val outPoint = input.getOutpoint
+      val relevantSubscriptions = outputSubscriptions(outPoint)
+      relevantSubscriptions.foreach { subscription =>
         subscription.outputSpent(ImmutableTransaction(tx))
       }
-      outputSubscriptions -= input.getOutpoint
+      if (relevantSubscriptions.nonEmpty) {
+        logger.info("{} was spent by {}", outPoint, tx.getHashAsString)
+        outputSubscriptions -= outPoint
+      }
     }
   }
 
-  override def notifyTransactionIsInBlock(txHash: Sha256Hash, block: StoredBlock,
-                                          blockType: NewBlockType, relativityOffset: Int): Boolean =
+  override def notifyTransactionIsInBlock(txHash: Sha256Hash,
+                                          block: StoredBlock,
+                                          blockType: NewBlockType,
+                                          relativityOffset: Int): Boolean = {
+    logger.trace(s"Transaction $txHash in block ${block.getHeader.getHashAsString} at height ${block.getHeight}")
+    updateConfirmationSubscriptions(txHash, block)
     confirmationSubscriptions.contains(txHash)
+  }
 
   private def notifyConfirmedTransactions(): Unit = {
     confirmationSubscriptions.foreach {
@@ -132,13 +152,15 @@ private[blockchain] class BlockchainNotifier(initialHeight: Int)
         } else {
           logger.info("{}: still waiting for more blocks", event)
         }
-      case _ =>
+
+      case (_, subscription) => logger.trace(s"Still waiting for $subscription")
     }
   }
 
   private def notifyHeight(): Unit = {
     heightSubscriptions.foreach { case subscription @ HeightSubscription(height, listener) =>
       if (currentHeight >= height) {
+        logger.info(s"Watched height $height was reached")
         heightSubscriptions -= subscription
         listener.heightReached(currentHeight)
       }
