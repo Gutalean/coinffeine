e532e1c94e4f3b78a80b13c2bcde2502bf657769
Sebastian Ortega <sortega@coinffeine.com>
2015-02-18 14:19:12 +0100

    Properly manage entries on MarketSubmissionActor
    Introduce the class Submission to keep invariants of the entries to be
    sent to the order book and remove some data-manipulation logic from
    the actors.

---
+++ coinffeine-peer/src/test/scala/coinffeine/peer/market/submission/SubmissionTest.scala
@@ -0,0 +1,29 @@
+package coinffeine.peer.market.submission
+
+import akka.actor.ActorRef
+
+import coinffeine.common.test.UnitTest
+import coinffeine.model.currency._
+import coinffeine.model.market._
+
+class SubmissionTest extends UnitTest {
+
+  val market = Market(Euro)
+  val requester = ActorRef.noSender
+  val orderBookEntry = OrderBookEntry.random(Bid, 10.BTC, Price(200.EUR))
+
+  "A submission" should "not have duplicated order ids" in {
+    val submissionEntry = Submission.Entry(requester, orderBookEntry)
+    an [IllegalArgumentException] shouldBe thrownBy {
+      Submission(market, Seq(submissionEntry, submissionEntry))
+    }
+  }
+
+  it should "replace older versions of entries" in {
+    val updatedEntry = orderBookEntry.copy[Euro.type](amount = 9.BTC)
+    Submission.empty(market)
+      .addEntry(requester, orderBookEntry)
+      .addEntry(requester, updatedEntry)
+      .entries should === (Seq(Submission.Entry(requester, updatedEntry)))
+  }
+}
