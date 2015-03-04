ea10978c0f88c91c8fddbbce82b7fc4c3135bb60
Alvaro Polo <apoloval@gmail.com>
2015-03-04 15:37:31 +0100

    Trigger an alarm on OKPay balance polling errors
    And recover from it when balance is obtained again.

---
+++ coinffeine-peer/src/main/scala/coinffeine/peer/payment/okpay/OkPayPollingAlarm.scala
@@ -0,0 +1,22 @@
+package coinffeine.peer.payment.okpay
+
+import coinffeine.alarms.{Severity, Alarm}
+
+case object OkPayPollingAlarm extends Alarm {
+
+  override def summary = "Cannot poll OKPay balance"
+
+  override def severity = Severity.Normal
+
+  override def whatHappened =
+    """
+      |There was an error while retrieving your balance from OKPay web services.
+      |The service might be unavailable, your user credentials might be invalid, or just
+      |the network connectivity is lost.
+    """.stripMargin
+
+  override def howToFix =
+    """
+      |Check your OKPay credentials (wallet ID & seed token) and your network connectivity.
+    """.stripMargin
+}
