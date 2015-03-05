0a80ca3bfe8c8c10763ec0bd7e4b5e5c95035bfa
Alvaro Polo <apoloval@gmail.com>
2015-03-05 11:55:20 +0100

    Replace buggy label with graphic by a `ImgLabel` class

--- coinffeine-gui/src/main/scala/coinffeine/gui/control/AlarmSummaryWidget.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/control/AlarmSummaryWidget.scala
@@ -1,20 +1,24 @@
 package coinffeine.gui.control
 
-import scalafx.scene.control.Label
-import scalafx.scene.image.ImageView
+import scalafx.scene.image.Image
 
 import coinffeine.alarms.Alarm
 import coinffeine.gui.beans.Implicits._
 import coinffeine.model.properties.Property
 
-class AlarmSummaryWidget(alarms: Property[Set[Alarm]]) extends Label {
+class AlarmSummaryWidget(alarms: Property[Set[Alarm]]) extends ImgLabel(AlarmSummaryWidget.OkImg, "") {
 
   styleClass += "alarm-summary"
   text <== alarms.map { a =>
     if (a.size > 0) s"${a.size} active alarms" else "No active alarms"
   }
-  graphic <== alarms.map { a =>
-    if (a.size == 0) new ImageView("/graphics/success-icon.png").delegate
-    else new ImageView("/graphics/alert-icon.png").delegate
+  image <== alarms.map { a =>
+    if (a.size == 0) AlarmSummaryWidget.OkImg.delegate
+    else AlarmSummaryWidget.WarningImg.delegate
   }
+}
+
+object AlarmSummaryWidget {
+  val OkImg = new Image("/graphics/success-icon.png")
+  val WarningImg = new Image("/graphics/alert-icon.png")
 }
