8d3a4c1a194802932fdae07df5a4e29ca5540c26
Alvaro Polo <apoloval@gmail.com>
2015-03-13 17:31:23 +0100

    Adapt adapt OKPay wallet data pane to faucet of Coinffeine TP

---
+++ coinffeine-gui/src/main/scala/coinffeine/gui/scene/styles/ColumnStyles.scala
@@ -0,0 +1,19 @@
+package coinffeine.gui.scene.styles
+
+import scalafx.geometry.HPos
+import scalafx.scene.layout.{Priority, ColumnConstraints}
+
+/** Styles applicable to columns. */
+object ColumnStyles {
+
+  trait FieldTitle { this: ColumnConstraints =>
+    halignment = HPos.Right
+    fillWidth = false
+    hgrow = Priority.Never
+  }
+
+  trait FieldValue { this: ColumnConstraints =>
+    fillWidth = true
+    hgrow = Priority.Always
+  }
+}
--- coinffeine-gui/src/main/scala/coinffeine/gui/scene/styles/PaneStyles.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/scene/styles/PaneStyles.scala
@@ -1,6 +1,6 @@
 package coinffeine.gui.scene.styles
 
-import scalafx.scene.layout.{Pane, HBox, VBox}
+import scalafx.scene.layout.{GridPane, Pane, HBox, VBox}
 
 /** Styles applicable to panes. */
 object PaneStyles {
@@ -11,11 +11,19 @@ object PaneStyles {
   /** A button row comprised by several separated buttons in a horizontal layout. */
   trait ButtonRow { this: HBox => styleClass += "button-row" }
 
+  /** A inner pane with some padding respect its parent. */
+  trait Inner { this: Pane => styleClass += "inner" }
+
+  /** A inner pane with empathized lateral margins. */
+  trait InnerWithMargins { this: Pane => styleClass += "inner-with-margins" }
+
   /** A box with some minor spacing */
   trait MinorSpacing { this: Pane => styleClass += "minor-spacing" }
 
   /** A set of text paragraphs with some spacing among them. */
   trait Paragraphs { this: VBox => styleClass += "paragraphs" }
+
+  trait SpacedGrid { this: GridPane => styleClass += "spaced-grid" }
 
   /** A Text field with a companion button on its right. */
   trait TextFieldWithButton { this: HBox => styleClass += "textfield-with-button" }
