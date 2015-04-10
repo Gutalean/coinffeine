374325369a3e2afe8cc70210560880fd679f28b6
Sebastian Ortega <sortega@coinffeine.com>
2015-04-10 09:45:52 +0200

    Simplify CoinffeineVersion
    Taking advantage of Ordering being a monoid

--- coinffeine-gui/src/main/scala/coinffeine/gui/application/updates/CoinffeineVersion.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/application/updates/CoinffeineVersion.scala
@@ -1,10 +1,7 @@
 package coinffeine.gui.application.updates
 
-import scala.annotation.tailrec
-import scalaz.Ordering.{EQ, GT, LT}
-import scalaz.std.anyVal._
-import scalaz.std.string._
-import scalaz.syntax.order._
+import scalaz.Ordering._
+import scalaz.Scalaz._
 
 import coinffeine.gui.application.BuildInfo
 
@@ -15,21 +12,13 @@ case class CoinffeineVersion(major: Int, minor: Int, revision: Int, tag: Option[
     s"v$major.$minor.$revision$printableTag"
   }
 
-  def isNewerThan(other: CoinffeineVersion): Boolean = isGreater(Seq(
-    this.major ?|? other.major,
-    this.minor ?|? other.minor,
-    this.revision ?|? other.revision,
-    compareTags(this.tag, other.tag)
-  ))
+  def isNewerThan(other: CoinffeineVersion): Boolean = compareTo(other) == GT
 
-  /** Combine orderings having the first ones more priority */
-  @tailrec
-  private def isGreater(orderings: Seq[scalaz.Ordering]): Boolean = orderings match {
-    case Seq(GT, _*) => true
-    case Seq(EQ) => false
-    case Seq(EQ, remaining @ _*) => isGreater(remaining)
-    case Seq(LT, _*) => false
-  }
+  def compareTo(other: CoinffeineVersion): scalaz.Ordering =
+    (this.major ?|? other.major) |+|
+    (this.minor ?|? other.minor) |+|
+    (this.revision ?|? other.revision) |+|
+    compareTags(this.tag, other.tag)
 
   /** Compare tags considering absence of tag as a newer version */
   private def compareTags(leftTag: Option[String], rightTag: Option[String]): scalaz.Ordering =
