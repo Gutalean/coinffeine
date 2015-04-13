4423a4abcebc9cfffd83b0f64722e563b614568e
Alvaro Polo <apoloval@gmail.com>
2015-04-13 15:57:15 +0200

    Add some styles for rounded buttons

--- coinffeine-gui/src/main/scala/coinffeine/gui/scene/styles/ButtonStyles.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/scene/styles/ButtonStyles.scala
@@ -10,4 +10,13 @@ object ButtonStyles {
 
   /** A button that represents an active action. */
   trait Action { this: Button => styleClass += "action-button" }
+
+  /** A rounded button. */
+  trait Rounded { this: Button => styleClass += "rounded-button" }
+
+  /** The details rounded button. */
+  trait Details extends Rounded { this: Button => styleClass += "details-button" }
+
+  /** The close rounded button. */
+  trait Close extends Rounded { this: Button => styleClass += "close-button" }
 }
