b836b6ea904f03695d495e120151a6e152f39088
Alvaro Polo <apoloval@gmail.com>
2015-04-13 10:25:43 +0200

    Add a new button style to represent active actions
    And apply it to the _new order_ button of operations view.

--- coinffeine-gui/src/main/scala/coinffeine/gui/scene/styles/ButtonStyles.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/scene/styles/ButtonStyles.scala
@@ -1,8 +1,13 @@
 package coinffeine.gui.scene.styles
 
+import scalafx.scene.control.Button
+
 /** Mixin traits to manage button styles. */
 object ButtonStyles {
 
   /** The URL to the stylesheet for these styles. */
   val StylesheetUrl = "/css/buttons.css"
+
+  /** A button that represents an active action. */
+  trait Action { this: Button => styleClass += "action-button" }
 }
