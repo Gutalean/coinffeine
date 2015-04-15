db5287a5c9500e64b5922090b1aeb28e2b0ecc29
Sebastian Ortega <sortega@coinffeine.com>
2015-04-15 18:35:16 +0200

    Remove broken unmaintained tests

--- coinffeine-gui/src/it/scala/coinffeine/gui/application/operations/DummyOrderValidation.scala
+++
@@ -1,9 +0,0 @@
-package coinffeine.gui.application.operations
-
-import coinffeine.gui.application.operations.validation.OrderValidation
-import coinffeine.model.currency.FiatCurrency
-import coinffeine.model.market.Order
-
-object DummyOrderValidation extends OrderValidation {
-  override def apply[C <: FiatCurrency](newOrder: Order[C]) = OrderValidation.OK
-}
