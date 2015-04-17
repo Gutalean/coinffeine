0e9470fb4758c6d0d19c85525c9751c56fc52b82
Sebastian Ortega <sortega@coinffeine.com>
2015-04-17 11:50:27 +0200

    Introduce a reusable style for pop-overs

--- coinffeine-gui/src/main/scala/coinffeine/gui/scene/styles/NodeStyles.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/scene/styles/NodeStyles.scala
@@ -1,7 +1,11 @@
 package coinffeine.gui.scene.styles
 
+import scalafx.Includes._
 import scalafx.scene.Node
+import scalafx.scene.input.MouseEvent
 import scalafx.scene.layout.Priority
+
+import org.controlsfx.control.PopOver
 
 /** Styles applicable to nodes. */
 object NodeStyles {
@@ -11,4 +15,21 @@ object NodeStyles {
 
   /** A node that expands vertically. */
   trait VExpand { this: Node => vgrow = Priority.Always }
+
+  /** A node that has a pop-over. */
+  trait Poppable { this: Node =>
+
+    /** The first time a pop-over is displayed derived classes are asked for its contents. */
+    def popOverContent: Node
+
+    private val popover = new PopOver(popOverContent) {
+      detachableProperty().set(false)
+      arrowSizeProperty().set(6)
+      arrowIndentProperty().set(4)
+    }
+
+    styleClass += "poppable"
+    onMouseEntered = { (ev: MouseEvent) => popover.show(this) }
+    onMouseExited = { (ev: MouseEvent) => popover.hide() }
+  }
 }
