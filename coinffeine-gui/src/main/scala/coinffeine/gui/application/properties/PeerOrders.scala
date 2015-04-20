0d4e30aed3a62730e6b30f71976eb3fdc41516b6
Sebastian Ortega <sortega@coinffeine.com>
2015-04-20 16:39:09 +0200

    Do sort orders by time of last change

--- coinffeine-gui/src/main/scala/coinffeine/gui/application/properties/PeerOrders.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/application/properties/PeerOrders.scala
@@ -3,6 +3,7 @@ package coinffeine.gui.application.properties
 import scala.concurrent.ExecutionContext
 import scalafx.collections.ObservableBuffer
 
+import coinffeine.model.market.AnyCurrencyOrder
 import coinffeine.peer.api.CoinffeineNetwork
 
 class PeerOrders(coinffeineNetwork: CoinffeineNetwork,
@@ -11,9 +12,13 @@ class PeerOrders(coinffeineNetwork: CoinffeineNetwork,
   implicit val exec = executor
 
   coinffeineNetwork.orders.onNewValue { (id, order) =>
-    find(_.idProperty.value == id) match {
-      case Some(orderProp) => orderProp.update(order)
-      case None => this += new MutableOrderProperties(order)
-    }
+    find(_.idProperty.value == id).fold(insertNewOrder(order))(_.update(order))
+  }
+
+  private def insertNewOrder(order: AnyCurrencyOrder): Unit = {
+    val index = takeWhile { props =>
+      order.lastChange.isAfter(props.orderProperty.value.lastChange)
+    }.size
+    insert(index, new MutableOrderProperties(order))
   }
 }
