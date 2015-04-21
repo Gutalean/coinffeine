a6e02ea816624036e0c797995a292e2aa57974fd
Alvaro Polo <apoloval@gmail.com>
2015-04-21 12:53:13 +0200

    Some enhancements in `GlyphToggle` control

--- coinffeine-gui/src/main/scala/coinffeine/gui/control/GlyphToggle.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/control/GlyphToggle.scala
@@ -1,12 +1,16 @@
 package coinffeine.gui.control
 
-import scalafx.beans.property.ObjectProperty
-import scalafx.scene.control.{ToggleGroup, ToggleButton}
+import scalafx.beans.property.{StringProperty, ObjectProperty}
+import scalafx.css.PseudoClass
+import scalafx.scene.control.{Label, ToggleGroup, ToggleButton}
+import scalafx.scene.layout.VBox
 
 import coinffeine.gui.beans.Implicits._
 
 /** A toggle button that used typography to render. */
-class GlyphToggle(group: ToggleGroup) extends ToggleButton {
+class GlyphToggle(initialText: String = "") extends VBox {
+
+  styleClass += "glyph-toggle"
 
   private val _icon = new ObjectProperty[GlyphIcon](this, "icon", GlyphIcon.Coinffeine)
   def icon: ObjectProperty[GlyphIcon] = _icon
@@ -14,7 +18,26 @@ class GlyphToggle(group: ToggleGroup) extends ToggleButton {
     _icon.value = value
   }
 
-  styleClass ++= Seq("glyph-icon", "glyph-toggle")
-  toggleGroup = group
-  text <== _icon.delegate.mapToString(_.letter.toString)
+  private val _text = new StringProperty(this, "text", initialText)
+  def text: StringProperty = _text
+  def text_=(value: String): Unit = { _text.value = value }
+
+  private val _toggleGroup = new ObjectProperty[ToggleGroup](this, "toggleGroup", new ToggleGroup())
+  def toggleGroup: ObjectProperty[ToggleGroup] = _toggleGroup
+  def toggleGroup_=(value: ToggleGroup): Unit = { _toggleGroup.value = value }
+
+  val toggle = new ToggleButton {
+    styleClass += "glyph-icon"
+    toggleGroup <== _toggleGroup.delegate.map(_.delegate)
+    text <== _icon.delegate.mapToString(_.letter.toString)
+  }
+
+  val label = new Label {
+    toggle.selected.onChange { (_, _, newValue) =>
+      delegate.pseudoClassStateChanged(PseudoClass("selected"), newValue)
+    }
+    text <== _text
+  }
+
+  content = Seq(toggle, label)
 }
