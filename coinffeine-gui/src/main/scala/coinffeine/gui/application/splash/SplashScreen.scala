9e6cf788cd58a0dffae3ea54ebb5677606e3b4d0
Sebastian Ortega <sortega@coinffeine.com>
2015-04-22 16:48:57 +0200

    Round splash screen borders

--- coinffeine-gui/src/main/scala/coinffeine/gui/application/splash/SplashScreen.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/application/splash/SplashScreen.scala
@@ -3,6 +3,7 @@ package coinffeine.gui.application.splash
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.control.Label
 import scalafx.scene.layout._
+import scalafx.scene.paint.Color
 import scalafx.stage.StageStyle
 
 import coinffeine.gui.application.updates.CoinffeineVersion
@@ -13,8 +14,9 @@ import coinffeine.gui.scene.styles.Stylesheets
 object SplashScreen {
 
   def displayOn(stage: PrimaryStage): Unit = {
-    stage.initStyle(StageStyle.UNDECORATED)
+    stage.initStyle(StageStyle.TRANSPARENT)
     stage.scene = new CoinffeineScene(Stylesheets.Splash) {
+      fill = Color.Transparent
       root = new VBox {
         id = "splash-root"
         content = Seq(
