96b947c5daeacbac65d68c84be276b10ad6c2c5b
Sebastian Ortega <sortega@coinffeine.com>
2015-04-23 12:28:34 +0200

    Change terminology from “withdraw” to “send”

--- coinffeine-gui/src/main/scala/coinffeine/gui/application/wallet/SendFundsForm.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/application/wallet/SendFundsForm.scala
@@ -14,22 +14,22 @@ import coinffeine.gui.scene.styles.Stylesheets
 import coinffeine.model.bitcoin.{Address, WalletProperties}
 import coinffeine.model.currency._
 
-class WithdrawFundsForm(props: WalletProperties) {
+class SendFundsForm(props: WalletProperties) {
 
-  import WithdrawFundsForm._
+  import SendFundsForm._
 
-  require(props.balance.get.isDefined, "cannot run withdraw funds form when balance is not defined")
+  require(props.balance.get.isDefined, "cannot run send funds form when balance is not defined")
 
   private val amount = new ObjectProperty[Bitcoin.Amount](this, "amount", 0.BTC)
   private val address = new ObjectProperty[Option[Address]](this, "address", None)
   private val submit = new BooleanProperty(this, "submit", false)
 
   private val content = new VBox() {
-    id = "wallet-withdraw-root-pane"
+    id = "wallet-send-root-pane"
     content = Seq(
       new VBox() {
         content = Seq(
-          new Label("Select the amount to withdraw"),
+          new Label("Select the amount to send"),
           new HBox() {
             val currencyField = new CurrencyTextField(0.BTC) {
               amount <== currencyValue
@@ -38,13 +38,12 @@ class WithdrawFundsForm(props: WalletProperties) {
               currencyField,
               new Button("Max") {
                 text <== props.balance.map {
-                  case Some(balance) => s"Max (${balance.amount})"
+                  case Some(balance) => s"Max (${balance.amount.format})"
                   case _ => "Max"
                 }
-                onAction = { action: Any =>
-                  val balance = props.balance.get
-                  if (balance.isDefined) {
-                    currencyField.text.value = balance.get.amount.value.toString()
+                onAction = () => {
+                  props.balance.get.foreach { balance =>
+                    currencyField.text.value = balance.amount.value.toString()
                   }
                 }
               }
@@ -55,7 +54,7 @@ class WithdrawFundsForm(props: WalletProperties) {
         content = Seq(
           new Label("Select the destination address"),
           new TextField() {
-            id = "wallet-withdraw-address-field"
+            id = "wallet-send-address-field"
             promptText = "Insert the destination Bitcoin address"
             address <== text.delegate.map { addr =>
               try { Some(new Address(null, addr)) }
@@ -69,7 +68,7 @@ class WithdrawFundsForm(props: WalletProperties) {
             maxWidth = Double.MaxValue
             onAction = { action: Any => close() }
           },
-          new Button("Withdraw") {
+          new Button("Send") {
             maxWidth = Double.MaxValue
             disable <== amount.delegate.mapToBool(a => !isValidAmount(a)) ||
               address.delegate.mapToBool(addr => addr.isEmpty)
@@ -87,10 +86,10 @@ class WithdrawFundsForm(props: WalletProperties) {
     amount.isPositive && amount <= props.balance.get.get.amount
 
   private val stage = new Stage(style = StageStyle.UTILITY) {
-    title = "Withdraw funds"
+    title = "Send funds"
     initModality(Modality.APPLICATION_MODAL)
     scene = new CoinffeineScene(Stylesheets.Wallet) {
-      root = WithdrawFundsForm.this.content
+      root = SendFundsForm.this.content
     }
     centerOnScreen()
   }
@@ -101,16 +100,12 @@ class WithdrawFundsForm(props: WalletProperties) {
 
   def show(): Result = {
     stage.showAndWait()
-    if (submit.value) Withdraw(amount.value, address.value.get)
-    else Cancel
+    if (submit.value) Send(amount.value, address.value.get) else CancelSend
   }
 }
 
-object WithdrawFundsForm {
-
+object SendFundsForm {
   sealed trait Result
-
-  case object Cancel extends Result
-
-  case class Withdraw(amount: Bitcoin.Amount, to: Address) extends Result
+  case object CancelSend extends Result
+  case class Send(amount: Bitcoin.Amount, to: Address) extends Result
 }
