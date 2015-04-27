bc4978710979e2e349d4c398161a4138c6cb5802
Sebastian Ortega <sortega@coinffeine.com>
2015-04-27 15:30:54 +0200

    Remove unused code

--- coinffeine-gui/src/main/scala/coinffeine/gui/scene/styles/OperationStyles.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/scene/styles/OperationStyles.scala
@@ -4,12 +4,6 @@ import coinffeine.model.market._
 
 object OperationStyles {
 
-  private val BuyStyleClass = "buy"
-  private val SellStyleClass = "sell"
-  private val RunningStyleClass = "running"
-  private val CompletedStyleClass = "completed"
-  private val FailedStyleClass = "failed"
-
   def stylesFor(order: AnyCurrencyOrder): Seq[String] = {
     val statusStyle = order.status match {
       case CompletedOrder => "completed"
