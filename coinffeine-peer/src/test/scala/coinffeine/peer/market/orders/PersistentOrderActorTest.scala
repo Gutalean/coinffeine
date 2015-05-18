8ea212db5c8b29afe8275d4a28cc214a980d6b25
Sebastian Ortega <sortega@coinffeine.com>
2015-05-18 11:06:12 +0200

    Persist exchange result in order actor journal

--- coinffeine-peer/src/test/scala/coinffeine/peer/market/orders/PersistentOrderActorTest.scala
+++ coinffeine-peer/src/test/scala/coinffeine/peer/market/orders/PersistentOrderActorTest.scala
@@ -1,7 +1,12 @@
 package coinffeine.peer.market.orders
 
+import org.joda.time.DateTime
+
 import coinffeine.common.akka.test.MockActor.MockStopped
-import coinffeine.model.exchange.AnyExchange
+import coinffeine.model.exchange._
+import coinffeine.model.order.OrderStatus
+import coinffeine.peer.exchange.ExchangeActor
+import coinffeine.peer.exchange.ExchangeActor.ExchangeFailure
 import coinffeine.peer.market.orders.archive.OrderArchive.{ArchiveOrder, OrderArchived}
 import coinffeine.peer.market.submission.SubmissionSupervisor.{KeepSubmitting, StopSubmitting}
 import coinffeine.protocol.messages.handshake.ExchangeRejection
@@ -34,6 +39,20 @@ class PersistentOrderActorTest extends OrderActorTest {
     restartOrder()
     exchangeActor.probe.expectMsgType[MockStopped]
     exchangeActor.expectCreation() shouldBe Seq(exchange)
+  }
+
+  it should "remember how an exchange finished" in new Fixture {
+    givenInMarketOrder()
+    gatewayProbe.relayMessageFromBroker(orderMatch)
+    givenSuccessfulFundsBlocking(orderMatch.exchangeId)
+    val Seq(exchange: HandshakingExchange[_]) = exchangeActor.expectCreation()
+    exchangeActor.probe.send(actor,
+      ExchangeActor.ExchangeSuccess(completedExchange.copy(timestamp = DateTime.now())))
+    expectProperty(_.exchanges(exchange.id).isCompleted)
+    properties.orders.set(order.id, null)
+
+    restartOrder()
+    expectProperty { _.status shouldBe OrderStatus.Completed }
   }
 
   it should "remember that the order was cancelled" in new Fixture {
