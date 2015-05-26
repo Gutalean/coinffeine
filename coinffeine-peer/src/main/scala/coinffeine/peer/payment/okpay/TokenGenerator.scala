c54da171167a10575613c4439f65417b33a87be8
Sebastian Ortega <sortega@coinffeine.com>
2015-05-26 15:48:09 +0200

    Use UTC on okpay token generation

--- coinffeine-peer/src/main/scala/coinffeine/peer/payment/okpay/TokenGenerator.scala
+++ coinffeine-peer/src/main/scala/coinffeine/peer/payment/okpay/TokenGenerator.scala
@@ -2,14 +2,15 @@ package coinffeine.peer.payment.okpay
 
 import java.security.MessageDigest
 
-import org.joda.time.DateTime
+import org.joda.time.{DateTime, DateTimeZone}
 
 /** Generates crypto tokens based on current datetime */
 class TokenGenerator(seedToken: String) {
 
   def build(currentTime: DateTime): String = {
-    val date = currentTime.toString("yyyyMMdd")
-    val hour = currentTime.toString("HH")
+    val referenceTime = currentTime.toDateTime(DateTimeZone.UTC)
+    val date = referenceTime.toString("yyyyMMdd")
+    val hour = referenceTime.toString("HH")
     val currentToken = String.format("%s:%s:%s", seedToken, date, hour)
     val hash = MessageDigest.getInstance("SHA-256").digest(currentToken.getBytes("UTF-8"))
     hash.map("%02X" format _).mkString
