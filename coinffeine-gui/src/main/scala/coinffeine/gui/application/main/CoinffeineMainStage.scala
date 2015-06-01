97f1c3c510d086593b3452c5482046b7349374d5
Alvaro Polo <apoloval@gmail.com>
2015-06-01 14:41:59 +0200

    Require confirmation before closing the app with running exchanges

--- coinffeine-gui/src/main/scala/coinffeine/gui/application/main/CoinffeineMainStage.scala
+++ coinffeine-gui/src/main/scala/coinffeine/gui/application/main/CoinffeineMainStage.scala
@@ -1,7 +1,10 @@
 package coinffeine.gui.application.main
 
+import scalafx.Includes._
+import scalafx.scene.control.{ButtonType, Alert}
+import scalafx.scene.control.Alert.AlertType
 import scalafx.scene.image.Image
-import scalafx.stage.{Stage, StageStyle}
+import scalafx.stage.{WindowEvent, Stage, StageStyle}
 
 import coinffeine.gui.application.operations.OperationsView
 import coinffeine.gui.application.operations.validation.DefaultOrderValidation
@@ -35,4 +38,26 @@ class CoinffeineMainStage(app: CoinffeineApp,
     settingsProvider = configProvider
   )
   icons.add(new Image(this.getClass.getResourceAsStream("/graphics/logo-256x256.png")))
+
+  onCloseRequest = { event: WindowEvent =>
+    if (!app.network.exchanges.forall(_.isCompleted)) {
+      val opt = new Alert(AlertType.Confirmation) {
+        title = "Close Coinffeine"
+        headerText = "Some exchanges are still running"
+        contentText =
+          "There are some orders that have running exchanges. If you quit the application " +
+          "the exchanges will not continue until the application is launched again. If it is " +
+          "not launched again after several hours, the exchange will be timed out and you " +
+          "will loose the deposits.\n\n" +
+          "It is highly recommended to wait for the exchanges to complete before closing " +
+          "the application.\n\n" +
+          "Do you really want to close Coinffeine?"
+        // FIXME: remove this dirty fix when dialog auto-sizing is fixed in Linux
+        //        See https://javafx-jira.kenai.com/browse/RT-40230
+        resizable = true
+        dialogPane.value.setPrefSize(450, 300)
+      }.showAndWait()
+      if (!opt.contains(ButtonType.OK)) { event.consume() }
+    }
+  }
 }
