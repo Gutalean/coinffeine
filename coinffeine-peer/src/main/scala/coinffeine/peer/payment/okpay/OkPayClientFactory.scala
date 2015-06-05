8912454b5262a4c30f5863d7e24705fcaa07828b
Sebastian Ortega <sortega@coinffeine.com>
2015-06-05 12:35:12 +0200

    Extract NotConfiguredClient and fix its warnings

--- coinffeine-peer/src/main/scala/coinffeine/peer/payment/okpay/OkPayClientFactory.scala
+++ coinffeine-peer/src/main/scala/coinffeine/peer/payment/okpay/OkPayClientFactory.scala
@@ -1,37 +1,22 @@
 package coinffeine.peer.payment.okpay
 
 import java.util.concurrent.atomic.AtomicReference
-import scala.concurrent.Future
-import scala.util.control.NoStackTrace
 
-import coinffeine.model.currency.{CurrencyAmount, FiatCurrency}
-import coinffeine.model.payment.PaymentProcessor._
-import coinffeine.peer.payment.okpay.OkPayClient.FeePolicy
 import coinffeine.peer.payment.okpay.ws.{OkPayWebService, OkPayWebServiceClient}
 
+/** Factory of OKPay clients.
+  *
+  * Web-service machinery is shared among clients for efficiency and, configuration is
+  * re-read every time to consider the latest settings.
+  */
 class OkPayClientFactory(lookupSettings: () => OkPaySettings)
   extends OkPayProcessorActor.ClientFactory {
 
   private val okPay = new OkPayWebService(lookupSettings().serverEndpointOverride)
   private val tokenCache = new AtomicReference[Option[String]](None)
 
-  object NotConfiguredClient extends OkPayClient {
-    private val error = new IllegalStateException(
-      "OKPay's user id and/or seed token are not configured") with NoStackTrace
-    override lazy val accountId: AccountId = throw error
-    override def findPaymentById(paymentId: PaymentId) = Future.failed(error)
-    override def findPaymentByInvoice(invoice: Invoice) = Future.failed(error)
-    override def currentBalances() = Future.failed(error)
-    override def sendPayment[C <: FiatCurrency](
-        to: AccountId, amount: CurrencyAmount[C], comment: String,
-        invoice: Invoice, feePolicy: FeePolicy) =
-      Future.failed(error)
-    override protected def executionContext = throw error
-  }
-
   override def build(): OkPayClient = {
-    val settings = lookupSettings()
-    settings.apiCredentials.fold[OkPayClient](NotConfiguredClient) { credentials =>
+    lookupSettings().apiCredentials.fold[OkPayClient](NotConfiguredClient) { credentials =>
       new OkPayWebServiceClient(
         okPay.service, tokenCache, credentials.walletId, credentials.seedToken)
     }
