439ee3004c4cd0a72afab9936371333dc30c6dc9
Sebastian Ortega <sortega@coinffeine.com>
2015-06-29 15:52:21 +0200

    Refactor Kryo serialization tests

--- coinffeine-peer/src/test/scala/coinffeine/peer/serialization/KeyPairSerializerTest.scala
+++ coinffeine-peer/src/test/scala/coinffeine/peer/serialization/KeyPairSerializerTest.scala
@@ -5,21 +5,21 @@ import coinffeine.model.bitcoin.test.CoinffeineUnitTestNetwork
 
 class KeyPairSerializerTest extends SerializerTest {
 
-  "Key pairs" should "support roundtrip Kryo serialization" in {
+  "Key pairs" should "support roundtrip Kryo serialization" in new Fixture {
     val keyPair = new KeyPair()
-    KeyPairUtils.equals(keyPair, keyPairSerializationRoundtrip(keyPair)) shouldBe true
+    KeyPairUtils.equals(keyPair, serializationRoundtrip(keyPair)) shouldBe true
   }
 
-  "Public keys" should "support roundtrip Kryo serialization" in {
+  "Public keys" should "support roundtrip Kryo serialization" in new Fixture {
     val publicKey = new KeyPair().publicKey
-    KeyPairUtils.equals(publicKey, keyPairSerializationRoundtrip(publicKey)) shouldBe true
+    KeyPairUtils.equals(publicKey, serializationRoundtrip(publicKey)) shouldBe true
   }
 
-  "Deterministic keys" should "support roundtrip Kryo serialization" in {
+  "Deterministic keys" should "support roundtrip Kryo serialization" in new Fixture {
     val wallet = new Wallet(CoinffeineUnitTestNetwork)
     val keyPair = wallet.freshReceiveKey()
     keyPair.canSign shouldBe true
-    val deserializedKeyPair = keyPairSerializationRoundtrip(keyPair)
+    val deserializedKeyPair = serializationRoundtrip[KeyPair](keyPair)
     KeyPairUtils.equals(keyPair, deserializedKeyPair) shouldBe true
     deserializedKeyPair.canSign shouldBe true
   }
