10526157497affa319a1c82764cae113cbe96aa3
Sebastian Ortega <sortega@coinffeine.com>
2015-07-12 16:28:01 +0200

    Introduce DepositUnlock entry type

--- coinffeine-model/src/main/scala/coinffeine/model/bitcoin/WalletActivity.scala
+++ coinffeine-model/src/main/scala/coinffeine/model/bitcoin/WalletActivity.scala
@@ -1,5 +1,7 @@
 package coinffeine.model.bitcoin
 
+import scala.collection.JavaConverters._
+
 import org.joda.time.DateTime
 
 import coinffeine.model.currency._
@@ -13,32 +15,60 @@ object WalletActivity {
   object EntryType {
     case object InFlow extends EntryType
     case object OutFlow extends EntryType
-    case object Deposit extends EntryType
+    case class DepositLock(id: ExchangeId) extends EntryType
+    case class DepositUnlock(id: ExchangeId) extends EntryType
   }
 
-  case class Entry(time: DateTime,
-                   tx: ImmutableTransaction,
-                   amount: BitcoinAmount,
-                   exchangeId: Option[ExchangeId]) {
-    def entryType: EntryType =
-      if (exchangeId.isDefined) EntryType.Deposit
-      else if (amount.isNegative) EntryType.OutFlow
-      else EntryType.InFlow
-  }
+  case class Entry(
+      entryType: EntryType,
+      time: DateTime,
+      tx: ImmutableTransaction,
+      amount: BitcoinAmount)
 
   def apply(
       deposits: Map[Hash, ExchangeId],
       wallet: Wallet,
-      transactions: MutableTransaction*): WalletActivity =
-    WalletActivity(transactions.map { tx =>
-      val exchangeId = deposits.get(tx.getHash)
-      Entry(
-        time = new DateTime(tx.getUpdateTime),
-        tx = ImmutableTransaction(tx),
-        amount =
-          if (exchangeId.isDefined) tx.getValueSentFromMe(wallet)
-          else tx.getValue(wallet),
-        exchangeId = exchangeId
-      )
-    })
+      transactions: MutableTransaction*): WalletActivity = {
+
+    def toEntry(tx: MutableTransaction): Entry =
+      createsDeposit(tx).map(deposit => toDepositLockEntry(deposit, tx)) orElse
+          spendsDeposit(tx).map(deposit => toDepositUnlockEntry(deposit, tx)) getOrElse
+          toRegularEntry(tx)
+
+    def createsDeposit(tx: MutableTransaction): Option[ExchangeId] = deposits.get(tx.getHash)
+
+    def toDepositLockEntry(deposit: ExchangeId, tx: MutableTransaction): Entry = entry(
+      entryType = EntryType.DepositLock(deposit),
+      tx = tx,
+      amount = tx.getValueSentFromMe(wallet)
+    )
+
+    def spendsDeposit(tx: MutableTransaction): Option[ExchangeId] = {
+      val spentTransactions = tx.getInputs.asScala.map(_.getOutpoint.getHash).toSet
+      deposits.collectFirst {
+        case (deposit, id) if spentTransactions.contains(deposit) => id
+      }
+    }
+
+    def toDepositUnlockEntry(deposit: ExchangeId, tx: MutableTransaction): Entry = entry(
+      entryType = EntryType.DepositUnlock(deposit),
+      tx = tx,
+      amount = tx.getValueSentToMe(wallet)
+    )
+
+    def toRegularEntry(tx: MutableTransaction): Entry = {
+      val amount = tx.getValue(wallet)
+      val entryType = if (amount.isNegative) EntryType.OutFlow else EntryType.InFlow
+      entry(entryType, tx, amount)
+    }
+
+    def entry(entryType: EntryType, tx: MutableTransaction, amount: Bitcoin.Amount) = Entry(
+      entryType = entryType,
+      time = new DateTime(tx.getUpdateTime),
+      tx = ImmutableTransaction(tx),
+      amount = amount
+    )
+
+    WalletActivity(transactions.map(toEntry))
+  }
 }
