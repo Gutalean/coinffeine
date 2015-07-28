e98db4b05e785313bb417f609aad627491712ec3
Sebastian Ortega <sortega@coinffeine.com>
2015-07-28 11:18:42 +0200

    Go monadic on the Cached type

--- coinffeine-model/src/test/scala/coinffeine/model/util/CachedTest.scala
+++ coinffeine-model/src/test/scala/coinffeine/model/util/CachedTest.scala
@@ -6,6 +6,8 @@ class CachedTest extends UnitTest {
 
   val freshGreet = Cached.fresh("hello")
   val staleGreet = Cached.stale("hello")
+  val freshName = Cached.fresh("Joe")
+  val staleName = Cached.stale("Joe")
 
   "A cached value" should "have a fresh attribute" in {
     freshGreet shouldBe 'fresh
@@ -21,4 +23,18 @@ class CachedTest extends UnitTest {
     freshGreet.map(_.length) shouldBe Cached.fresh(5)
     staleGreet.map(_.length) shouldBe Cached.stale(5)
   }
+
+  it should "be flatMapped to a fresh result if all inputs are fresh" in {
+    composeGreeting(freshGreet, freshName) shouldBe Cached.fresh("hello Joe!")
+  }
+
+  it should "be flatMapped to a stale result if some input is stale" in {
+    composeGreeting(staleGreet, freshName) shouldBe Cached.stale("hello Joe!")
+    composeGreeting(freshGreet, staleName) shouldBe Cached.stale("hello Joe!")
+  }
+
+  private def composeGreeting(greet: Cached[String], name: Cached[String]) = for {
+    greet <- greet
+    name <- name
+  } yield s"$greet $name!"
 }
